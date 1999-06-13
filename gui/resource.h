5faec239ff2b6d0d387897698838104885836135
richardicooper <richardiancooper@gmail.com>
1999-06-13 15:48:30 +0000

    RIC: New resource.h

--- gui/resource.h
+++ gui/resource.h
@@ -1,16 +1,17 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Developer Studio generated include file.
-// Used by Script1.rc
+// Used by script1.rc
 //
-#define IDD_DIALOG1                     101
-#define IDC_CURSOR1                     102
-#define IDC_CURSOR2                     103
+#define IDC_POINTER_COPY                105
+#define IDC_CURSOR1                     106
+#define IDC_CURSOR2                     107
+#define IDC_CURSOR3                     108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
