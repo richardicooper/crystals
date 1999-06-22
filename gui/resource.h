62e04bcc8e91105e0b100984f665ee25c2004663
richardicooper <richardiancooper@gmail.com>
1999-06-22 13:22:41 +0000

    RIC: This file is written by the DevStudio resource editor.

--- gui/resource.h
+++ gui/resource.h
@@ -2,6 +2,8 @@
 // Microsoft Developer Studio generated include file.
 // Used by script1.rc
 //
+#define IDB_SPLASH                      102
+#define IDI_ICON1                       104
 #define IDC_POINTER_COPY                105
 #define IDC_CURSOR1                     106
 #define IDC_CURSOR2                     107
@@ -11,7 +13,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
