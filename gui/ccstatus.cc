18ba009ff67c7366e4928c57f0d8f28debb51abb
richardicooper <richardiancooper@gmail.com>
2001-03-08 15:22:11 +0000

    New flag signalling that the model window is in a ZOOMED in state. Call to UpdateToolbars, when status changes, updates toolbar buttons, normal buttons and windows with the DISABLEIF, ENABLEIF flags.

--- gui/ccstatus.cc
+++ gui/ccstatus.cc
@@ -7,17 +7,18 @@
 //   Filename:  CcStatus.cc
 //   Authors:   Richard Cooper and Ludwig Macko
 //   Created:   26.2.1998 9:36 Uhr
-//   Modified:  11.3.1998 11:36 Uhr
+//   $Log: not supported by cvs2svn $
 
-#include	"crystalsinterface.h"
+#include    "crystalsinterface.h"
 #include "ccstring.h"
+#include "cccontroller.h"
 #include        "crconstants.h"
-#include	"ccstatus.h"
-#include	"cctokenlist.h"
+#include    "ccstatus.h"
+#include    "cctokenlist.h"
 
 CcStatus::CcStatus()
 {
-	statusFlags = 0;
+    statusFlags = 0;
 }
 
 CcStatus::~CcStatus()
@@ -26,30 +27,41 @@ CcStatus::~CcStatus()
 
 void CcStatus::SetNumSelectedAtoms(int nSelected)
 {
-	//nSelected;
-	UnSetBit(16,&statusFlags);
-	UnSetBit(17,&statusFlags);
-	UnSetBit(18,&statusFlags);
-	UnSetBit(19,&statusFlags);
-	UnSetBit(20,&statusFlags);
-	if (nSelected == 0)
-		SetBit(16,&statusFlags);
-	else if(nSelected == 1)
-		SetBit(17,&statusFlags);
-	else if(nSelected == 2)
-		SetBit(18,&statusFlags);
-	else if(nSelected == 3)
-		SetBit(19,&statusFlags);
+    //nSelected;
+    UnSetBit(16,&statusFlags);
+    UnSetBit(17,&statusFlags);
+    UnSetBit(18,&statusFlags);
+    UnSetBit(19,&statusFlags);
+    UnSetBit(20,&statusFlags);
+    if (nSelected == 0)
+        SetBit(16,&statusFlags);
+    else if(nSelected == 1)
+        SetBit(17,&statusFlags);
+    else if(nSelected == 2)
+        SetBit(18,&statusFlags);
+    else if(nSelected == 3)
+        SetBit(19,&statusFlags);
 
-	if(nSelected >= 3)
-		SetBit(20,&statusFlags);
+    if(nSelected >= 3)
+        SetBit(20,&statusFlags);
+
+    UpdateToolBars();
 }
+
+void CcStatus::SetZoomedFlag(bool zoomed)
+{
+  if (zoomed)  SetBit(21,&statusFlags);
+  else       UnSetBit(21,&statusFlags);
+
+  UpdateToolBars();
+}
+
 
 Boolean CcStatus::ShouldBeEnabled(int enableFlags, int disableFlags)
 {
-	if( disableFlags & statusFlags )           //If any bits match, then should be disabled.
+    if( disableFlags & statusFlags )           //If any bits match, then should be disabled.
             return false;
-	if ( enableFlags & ( ~statusFlags) ) //If any 0's in the statusFlag match any 1's in the enableFlags, then should be disabled.
+    if ( enableFlags & ( ~statusFlags) ) //If any 0's in the statusFlag match any 1's in the enableFlags, then should be disabled.
             return false;
 
       return true;   //Otherwise, enable.
@@ -58,132 +70,144 @@ Boolean CcStatus::ShouldBeEnabled(int enableFlags, int disableFlags)
 
 void CcStatus::SetBit(int i, int* theFlag)
 {
-	int addbit = 1;
-	for (int j = 0; j < i; j++) addbit *= 2;
+    int addbit = 1;
+    for (int j = 0; j < i; j++) addbit *= 2;
 
-	*theFlag |= addbit;
+    *theFlag |= addbit;
 }
 
 void CcStatus::UnSetBit(int i, int * theFlag)
 {
-	int addbit = 1;
-	for (int j = 0; j < i; j++) addbit *= 2;
+    int addbit = 1;
+    for (int j = 0; j < i; j++) addbit *= 2;
 
-	*theFlag &= (~addbit);
+    *theFlag &= (~addbit);
 }
 
 
 
 int CcStatus::CreateFlag(CcString text)
 {
-	int returnFlag = 0;
+    int returnFlag = 0;
 
-	int start = 0, i;
-	Boolean inSpace = true;
-	
-	for (i=1; i < text.Len()+1; i++ )
-	{
-		if ( text[i-1] == ' ' || text[i-1] == '\t' || text[i-1] == '\r' || text[i-1] == '\n' ) 
-		{
-			if ( ! inSpace )                  // end of item
-			{
-				// Set the appropriate flag bit
-				int bit = GetBitByToken(text.Sub(start,i-1));
-				if(bit >= 0)
-					SetBit(bit,&returnFlag);
-				// init values
-				start = 0;
-				inSpace = true;
-			}
-		}
-		else if ( inSpace )  	         	  // start of item
-		{
-			start = i;
-			inSpace = false;
-		}
-	}
+    int start = 0, i;
+    Boolean inSpace = true;
 
-	// Check for last item
-	if ( ! inSpace && start != 0 )
-	{
-		// add item to token list
-		int bit = GetBitByToken(text.Sub(start,i-1));
-		if(bit >= 0)
-			SetBit(bit,&returnFlag);
-	}
+    for (i=1; i < text.Len()+1; i++ )
+    {
+        if ( text[i-1] == ' ' || text[i-1] == '\t' || text[i-1] == '\r' || text[i-1] == '\n' )
+        {
+            if ( ! inSpace )                  // end of item
+            {
+                // Set the appropriate flag bit
+                int bit = GetBitByToken(text.Sub(start,i-1));
+                if(bit >= 0)
+                   SetBit(bit,&returnFlag);
+                // init values
+                start = 0;
+                inSpace = true;
+            }
+        }
+        else if ( inSpace )                   // start of item
+        {
+            start = i;
+            inSpace = false;
+        }
+    }
 
-	return returnFlag;
+    // Check for last item
+    if ( ! inSpace && start != 0 )
+    {
+        // add item to token list
+        int bit = GetBitByToken(text.Sub(start,i-1));
+        if(bit >= 0)
+            SetBit(bit,&returnFlag);
+    }
+
+    return returnFlag;
 }
 
 int CcStatus::GetBitByToken(CcString token)
 {
-//	 newway           if(token==CcString(bS0))  return 0;
-//	 oldway           if(token==CcString(bS1))  {SetBit(1,flag);return;}
+//   newway           if(token==CcString(bS0))  return 0;
+//   oldway           if(token==CcString(bS1))  {SetBit(1,flag);return;}
 #define CHECKTOKEN(a) if(token==CcString(bS##a))return a;
-	CHECKTOKEN(0)
-	CHECKTOKEN(1)
-	CHECKTOKEN(2)
-	CHECKTOKEN(3)
-	CHECKTOKEN(4)
-	CHECKTOKEN(5)
-	CHECKTOKEN(6)
-	CHECKTOKEN(7)
-	CHECKTOKEN(8)
-	CHECKTOKEN(9)
-	CHECKTOKEN(10)
-	CHECKTOKEN(11)
-	CHECKTOKEN(12)
-	CHECKTOKEN(13)
-	CHECKTOKEN(14)
-	CHECKTOKEN(15)
-	CHECKTOKEN(16)
-	CHECKTOKEN(17)
-	CHECKTOKEN(18)
-	CHECKTOKEN(19)
-	CHECKTOKEN(20)
-	CHECKTOKEN(21)
-	CHECKTOKEN(22)
-	CHECKTOKEN(23)
-	CHECKTOKEN(24)
-	CHECKTOKEN(25)
-	CHECKTOKEN(26)
-	CHECKTOKEN(27)
-	CHECKTOKEN(28)
-	CHECKTOKEN(29)
-	CHECKTOKEN(30)
-	CHECKTOKEN(31)
-	return -1; //Tokenn not matched.
+    CHECKTOKEN(0)
+    CHECKTOKEN(1)
+    CHECKTOKEN(2)
+    CHECKTOKEN(3)
+    CHECKTOKEN(4)
+    CHECKTOKEN(5)
+    CHECKTOKEN(6)
+    CHECKTOKEN(7)
+    CHECKTOKEN(8)
+    CHECKTOKEN(9)
+    CHECKTOKEN(10)
+    CHECKTOKEN(11)
+    CHECKTOKEN(12)
+    CHECKTOKEN(13)
+    CHECKTOKEN(14)
+    CHECKTOKEN(15)
+    CHECKTOKEN(16)
+    CHECKTOKEN(17)
+    CHECKTOKEN(18)
+    CHECKTOKEN(19)
+    CHECKTOKEN(20)
+    CHECKTOKEN(21)
+    CHECKTOKEN(22)
+    CHECKTOKEN(23)
+    CHECKTOKEN(24)
+    CHECKTOKEN(25)
+    CHECKTOKEN(26)
+    CHECKTOKEN(27)
+    CHECKTOKEN(28)
+    CHECKTOKEN(29)
+    CHECKTOKEN(30)
+    CHECKTOKEN(31)
+    return -1; //Tokenn not matched.
 }
 
 
 void CcStatus::ParseInput(CcTokenList * tokenList)
 {
-	Boolean moreTokens = true;
-	while (moreTokens)
-	{
-		switch(tokenList->GetDescriptor(kStatusClass))
-		{
-			case kTSetStatus:
-			{
-				tokenList->GetToken();
-				int setFlags = CreateFlag(tokenList->GetToken());
-				statusFlags |= setFlags;
-				break;
-			}
-			case kTUnSetStatus:
-			{
-				tokenList->GetToken();
-				int unSetFlags = CreateFlag(tokenList->GetToken());
-				statusFlags &= (~unSetFlags);
-				break;
-			}
-			default:
-			{
-				moreTokens = false;
-				break;
-			}
+    Boolean moreTokens = true;
+    while (moreTokens)
+    {
+        switch(tokenList->GetDescriptor(kStatusClass))
+        {
+            case kTSetStatus:
+            {
+                tokenList->GetToken();
+                int setFlags = CreateFlag(tokenList->GetToken());
+                statusFlags |= setFlags;
+                break;
+            }
+            case kTUnSetStatus:
+            {
+                tokenList->GetToken();
+                int unSetFlags = CreateFlag(tokenList->GetToken());
+                statusFlags &= (~unSetFlags);
+                break;
+            }
+            default:
+            {
+                UpdateToolBars();
+                moreTokens = false;
+                break;
+            }
 
-		}
-	}
+        }
+    }
+
+}
+
+void CcStatus::UpdateToolBars()
+{
+  // Unlike menus, the toolbars don't recieve events telling them
+  // to update during idle time.
+  // So we'll do it from here.
+
+  (CcController::theController)->UpdateToolBars();
+
 
 }
--- gui/ccstatus.h
+++ gui/ccstatus.h
@@ -7,22 +7,22 @@
 //   Filename:  CcStatus.h
 //   Authors:   Richard Cooper and Ludwig Macko
 //   Created:   26.2.1998 9:36 Uhr
-//   Modified:  11.3.1998 11:36 Uhr
+//   $Log: not supported by cvs2svn $
 
-#ifndef		__CcStatus_H__
-#define		__CcStatus_H__
+#ifndef     __CcStatus_H__
+#define     __CcStatus_H__
 //Insert your own code here.
 
-//End of user code.         
+//End of user code.
 
 //Updated by crystals thread.
-#define bS0		"L1"
-#define bS1		"L2"
-#define bS2		"L3"
-#define bS3		"L5"
-#define bS4		"L6"
-#define bS5		"IN"
-#define bS6		"QS"
+#define bS0     "L1"
+#define bS1     "L2"
+#define bS2     "L3"
+#define bS3     "L5"
+#define bS4     "L6"
+#define bS5     "IN"
+#define bS6     "QS"
 #define bS7
 #define bS8
 #define bS9
@@ -33,12 +33,12 @@
 #define bS14
 #define bS15
 //Reserved for interface.
-#define bS16	"SEL0"
-#define bS17	"SEL1"
-#define bS18	"SEL2"
-#define bS19	"SEL3"
-#define bS20	"SEL>=3"
-#define bS21
+#define bS16    "SEL0"
+#define bS17    "SEL1"
+#define bS18    "SEL2"
+#define bS19    "SEL3"
+#define bS20    "SEL>=3"
+#define bS21    "ZOOMED"
 #define bS22
 #define bS23
 #define bS24
@@ -52,29 +52,52 @@
 
 class CcTokenList;
 
-class	CcStatus
+//There is only one CcStatus object, it belongs to CcController.
+
+class   CcStatus
 {
-	public:
-		void ParseInput(CcTokenList* tokenList);
-		void UnSetBit(int i, int* theFlags);
-		int CreateFlag(CcString theFlags);
-		Boolean ShouldBeEnabled(int enableFlags, int disableFlags);
-		// methods
-			CcStatus();
-			~CcStatus();
-			void SetNumSelectedAtoms(int n);
-		
-	private:
-		int GetBitByToken(CcString token);
-		void SetBit(int i, int* theFlag);
-		//attributes
-		
-		int statusFlags;
+  public:
+
+// methods
+
+    CcStatus();
+    ~CcStatus();
+
+// sets and unsets flags as notified by CRYSTALS.
+
+    void ParseInput(CcTokenList* tokenList);
+
+
+// sets and unsets flags as notified by the MODEL VIEW.
+
+    void SetNumSelectedAtoms(int n);
+    void SetZoomedFlag(bool zoomed);
+
+// Creates a bit flag for menu items/toolbars based on the
+// ENABLEIF and DISABLEIF strings they are passed.
+
+    int CreateFlag(CcString theFlags);
+
+// Checks a menu/toolbar item against the current status to determine
+// whether it should be enabled.
+
+    bool ShouldBeEnabled(int enableFlags, int disableFlags);
+
+  private:
+
+//methods
+    int GetBitByToken(CcString token);
+    void UnSetBit(int i, int* theFlags);
+    void SetBit(int i, int* theFlag);
+    void UpdateToolBars();
+
+//attributes
+    int statusFlags;
 
 };
 
-#define kSSetStatus		"STATSET"
-#define kSUnSetStatus		"STATUNSET"
+#define kSSetStatus     "STATSET"
+#define kSUnSetStatus       "STATUNSET"
 
 enum
 {
