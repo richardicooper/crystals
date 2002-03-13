c4263680b66a8698e698c755527d7f1707d31f58
richardicooper <richardiancooper@gmail.com>
2002-03-13 12:33:28 +0000

    Call XGUIUP in between EVERY instruction.

--- crystals/crystals.F
+++ crystals/crystals.F
@@ -1,4 +1,16 @@
 C $Log: not supported by cvs2svn $
+C Revision 1.10  2001/10/08 12:25:58  ckp2
+C
+C All program sub-units now RETURN to the main CRYSTL() function inbetween commands.
+C The changes made are: in every sub-program the GOTO's that used to loop back for
+C the next KNXTOP command have been changed to RETURN's. In the main program KNXTOP is now
+C called at the top of the loop, but first the current ProgramName (KPRGNM) array is cleared
+C to ensure the KNXTOP knows that it is not in the correct sub-program already. (This
+C is the way KNXTOP worked on the very first call within CRYSTALS).
+C
+C We now have one location (CRYSTL()) where the program flow returns between every command. I will
+C put this to good use soon.
+C
 C Revision 1.9  2001/02/26 10:24:14  richard
 C Added changelog to top of file
 C
@@ -173,6 +185,8 @@ C            STOP 'ERROR OPENING STARTUP FILE'
       ENDIF
 C
 1210  CONTINUE
+
+      CALL XGUIUP(0)
 
       CALL XFILL(IB,KPRGNM,17) !No longer in a program sub-unit.
       I = KNXTOP ( LSTOP,LSTNO,ICLASS )
