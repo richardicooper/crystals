02defc774cb2d769723e4973ac2968164d620a81
richardicooper <richardiancooper@gmail.com>
2003-02-19 12:05:11 +0000

    Created to make a matrix of statistical results from the reflections. This is an optimization to the previous table structure.

---
+++ bits/SpaceGroup/Stats.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Stats.cpp
+ *  Space Groups
+ *
+ *  Created by Stefan Pantos on Wed Feb 19 2003.
+ *  Copyright (c) 2003 . All rights reserved.
+ *
+ */
+
+#include "Stats.h"
+
---
+++ bits/SpaceGroup/Stats.h
@@ -0,0 +1,10 @@
+/*
+ *  Stats.h
+ *  Space Groups
+ *
+ *  Created by Stefan Pantos on Wed Feb 19 2003.
+ *  Copyright (c) 2003 . All rights reserved.
+ *
+ */
+
+
