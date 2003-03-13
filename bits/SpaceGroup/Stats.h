0159886914f94ff1a71a46aff92a6c503b6357b5
richardicooper <richardiancooper@gmail.com>
2003-03-13 15:55:56 +0000

    Add the structure ElemStats to and the class prototype Stats. This creates a matrix of statistics for all the heading conditions and the row conditions.

--- bits/SpaceGroup/Stats.h
+++ bits/SpaceGroup/Stats.h
@@ -7,4 +7,39 @@
  *
  */
 
+#include "CrystalSystem.h"
 
+typedef struct ElemStats
+{
+    float tNonMTotInt;	//Total intensity non-matched.
+    float tMTotInt;	//Total intensity matched. 
+    int tNumNonM;	//Number non-matched
+    int tNumM;	//Number matched
+    int tNumNonMLsInt;	//Number Int<3*sigma non-matched
+    int tNumNonMGrInt;	//Number Int>=3*sigma non-matched
+    float tRating1;
+    float tRating2; 
+}ElemStats;
+
+class Stats
+{
+    private:
+        int iTotalNum;
+        float iTotalIntensity;
+        ElemStats* iStats;	//Cells of the stats.
+        Headings* iHeadings;
+        Conditions* iConditions;
+	void outputHeadings(ostream& pStream);
+        void outputRow(int pRow, ostream& pStream);
+        static float evaluationFunction(float pX, float AbsentM, float AbsentSD, float PresentM, float PresentSD);
+    public:
+        Stats(Headings* tHeadings, Conditions* tConditions);
+        ~Stats();
+        void addReflectionRows(int pColumn, Reflection* pReflection, Matrix<float>* pHKLM);
+        void addReflection(Reflection* tReflection);
+        void calProbs();			//Calculates all the probabilites for all the cells.
+        ElemStats* getElem(int pHeadIndex, int pCondIndex)
+        ostream& output(ostream& pStream);
+};
+
+ostream& operator<<(ostream& pStream, Stats& pStats);
