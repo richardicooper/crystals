7e660d179ba046d74207a16acb09cffa0d0cfdef
richardicooper <richardiancooper@gmail.com>
2003-03-21 15:42:55 +0000

    Change Monoclinic to MonoclinicB

--- bits/SpaceGroup/main.cpp
+++ bits/SpaceGroup/main.cpp
@@ -35,94 +35,156 @@
  * };
  */
  
+//#if defined(_WIN32)
+//#include "stdafx.h"
+//#endif
+
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
-#include <regex.h>
-#include "Collections.h"
 #include "HKLData.h"
 #include "Exceptions.h"
-#include "Matrices.h"
 #include "CrystalSystem.h"
-#include <fstream>
+#include "Stats.h"
+#include "UnitCell.h"
 using namespace std;
 
-void readInTable(filebuf& pFile, Table* pTable);
+typedef struct RunStruct
+{
+    char* iFileName;		//File name for the hkl data.
+    char* iTablesFile;		//The file name of the tables file.
+    bool iChiral;		//false is not nessaseraly chiral. false chiral
+    char* iCrystalSys;		//Crystal System
+}RunStruct;
 
-int main (int argc, const char * argv[]) 
-{  
-            try
-            {
-                HKLData* tReflections = new HKLData("/Users/stefan/Documents/Programming/C++/SpaceGroup/Test files/co1c.hkl");
-                //tReflections->centeringTypeInfo();
-
-                Headings tHeadings;
-                Conditions tConditions;
-                
-                filebuf tFile;
-                tFile.open("/Users/stefan/Documents/Programming/C++/SpaceGroup/Data/Tables.txt", ios::in);
-                
-                tHeadings.readFrom(tFile);
-                tConditions.readFrom(tFile);
-            
-                Tables tTables(&tHeadings, &tConditions);
-                tTables.readFrom(tFile);
-                tFile.close(); 
-        
-                char tSystem[] = "MONOCLINIC";
-                Table* tTable = tTables.findTable(tSystem);
-                int tNumRefl = tReflections->numberOfReflections();
-                for (int i = 0; i < tNumRefl; i++)
-                {
-                    Reflection* tReflection = tReflections->getReflection(i);
-                    tTable->addReflection(tReflection);
-                }
-                
-             //   cout << tHeadings << "\n";
-             //   cout << tConditions << "\n";
-                tTable->outputColumn(cout, 0, &tHeadings, &tConditions);
-                tTable->outputColumn(cout, 1, &tHeadings, &tConditions);
-                tTable->outputColumn(cout, 2, &tHeadings, &tConditions);
-                cout << tTables << "\n";
-
-                delete tReflections;
-            }
-            catch(MyException eE)
-            {
-                cout << eE.what() << "\n";
-            }            
+void runTest(RunStruct *pRunData)
+{
+    std::cout << "Reading in tables...";
+    Tables tTables(pRunData->iTablesFile);
+    std::cout << "\nReading in hkl data...";
+    HKLData tHKL(pRunData->iFileName);
+//    std::cout << "\nReading in unit cell...";
+    std::cout << "\nCalculating probabilities...";
+    Table* tTable = tTables.findTable(pRunData->iCrystalSys);
+    Stats tStats(tTables.getHeadings(), tTables.getConditions());
+    int tNumRef = tHKL.numberOfReflections();
+    for (int i = 0; i < tNumRef; i++)
+    {
+        Reflection* tReflection = tHKL.getReflection(i);
+        tStats.addReflection(tReflection);
+    }
+    tStats.calProbs();
+    std::cout << "\nRanking space groups...";
+    RankedSpaceGroups tRankings(*tTable, tStats);
+    std::cout << "\n" << tRankings << "\n";
 }
-/*
+
+void initRunStruct(RunStruct *pRunStruct)
+{
+    pRunStruct->iTablesFile = new char[strlen("./Tables.txt"+1)];
+    strcpy(pRunStruct->iTablesFile, "./Tables.txt");
+    pRunStruct->iFileName = new char[255];
+    pRunStruct->iFileName[0] = 0;
+    pRunStruct->iCrystalSys = new char[20];
+    pRunStruct->iChiral = false;
+}
+
+bool handleArg(int *pPos, int pMax, const char * argv[], RunStruct *pRunData)
+{
+    if (strcmp(argv[(*pPos)], "-c") == 0)
+    {
+        pRunData->iChiral = true;
+        (*pPos)++;
+        return true;
+    }
+    else if (strcmp(argv[*pPos], "-f")==0)
+    {
+        (*pPos)++;
+        if (*pPos < pMax && argv[*pPos][0] != '-')
+        {
+            strcpy(pRunData->iFileName, argv[*pPos]);
+            (*pPos)++;
+            return true;
+        }
+    }
+    else if (strcmp(argv[*pPos], "-t")==0)
+    {
+        (*pPos)++;
+        if (*pPos < pMax && argv[*pPos][0] != '-')
+        {
+            delete pRunData->iTablesFile;
+            pRunData->iTablesFile = new char[strlen(argv[*pPos])+1];
+            strcpy(pRunData->iTablesFile, argv[*pPos]);
+            (*pPos)++;
+            return true;
+        }
+    }
+    else if (strcmp(argv[*pPos], "-s")==0)
+    {
+        (*pPos)++;
+        if (*pPos < pMax && argv[*pPos][0] != '-')
+        {
+            delete pRunData->iTablesFile;
+            pRunData->iTablesFile = new char[strlen(argv[*pPos])+1];
+            strcpy(pRunData->iTablesFile, argv[*pPos]);
+            (*pPos)++;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool handleArgs(int pArgc, const char * argv[], RunStruct *pRunData)
+{
+    int tCount = 1;
+    while (tCount < pArgc)
+    {
+        if (!handleArg(&tCount, pArgc, argv, pRunData))
+        {
+            std::cout << "Usage: " << argv[0] << "[-f hklfile] [-t tablefile] [-c] [-s system]\n";
+            std::cout << "-f hklfile: The path of the hkl file to read in.\n";
+            std::cout << "-t tablefile: The path of the table file.\n";
+            std::cout << "-c: The structure is chiral.\n";
+            std::cout << "-s system#: The crystal system table to use.\n";
+            std::cout << "Valid values for system are:\n" <<
+            "\t0: Triclinic" <<
+            "\t1: MonoclinicA\n" <<
+            "\t2: MonoclinicB\n" <<
+            "\t3: MonoclinicC\n" <<
+            "\t4: Orthorhombic\n" <<
+            "\t5: Tetragonal\n" <<
+            "\t6: Trigonal\n" <<
+            "\t7: Hexagonal\n" <<
+            "\t8: Cubic\n";
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, const char * argv[]) 
+{
+    RunStruct tRunStruct;
+    initRunStruct(&tRunStruct);
+    if (!handleArgs(argc, argv, &tRunStruct))
+    {
+        return 0;
+    }
+    if (tRunStruct.iFileName[0] == 0)
+    {
+        std::cout << "Enter hkl file path: ";
+        cin >> tRunStruct.iFileName;
+    }
+    strcpy(tRunStruct.iCrystalSys, getCrystalSystem());
     try
     {
-        char* tNextLine;
-        char* tPrevLine;
-
-        Headings tHeadings;
-        Conditions tConditions;
-        
-            filebuf tFile;
-            tFile.open("/Users/stefan/Desktop/Tables.txt", ios::in);
-            
-            tHeadings.readFrom(tFile);
-            tConditions.readFrom(tFile);
-        
-            Tables tTables(&tHeadings, &tConditions);
-            tTables.readFrom(tFile);
-            tFile.close(); 
-            
-            
-        
-        cout << tHeadings << "\n";
-        cout << tConditions << "\n";
-        cout << tTables << "\n";
+        runTest(&tRunStruct);
     }
-    catch (MyException& e)
+    catch (MyException eE)
     {
-        cout << e.what() << "\n";
-    }
-    return 0;
-}*/
-
-
+        std::cout << eE.what() << "\n";
+    }   
+    delete tRunStruct.iFileName;
+    delete tRunStruct.iTablesFile;
+    delete tRunStruct.iCrystalSys;
+}
