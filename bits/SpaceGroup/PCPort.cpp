5c6e908867dc2c9157f13fa17c1dfe9a2ac633de
richardicooper <richardiancooper@gmail.com>
2003-05-28 16:25:32 +0000

    Added for Windows support

---
+++ bits/SpaceGroup/PCPort.cpp
@@ -0,0 +1,66 @@
+//#include "stdafx.h"
+
+#if defined(_WIN32)
+#include <string.h>
+#include "PCPort.h"
+#include <sys/types.h>
+#include <sys/timeb.h>
+
+void bzero(void* pPoint, long pBytes)
+{
+	for (long i = 0; i < pBytes; i++)
+	{
+		((char*)pPoint)[i] = 0;
+	}
+}
+
+char* index(char* pString, int pChar)
+{
+	short tStrLen = (int)strlen(pString);
+	for (short i = 0; i < tStrLen; i++)
+	{	
+		if (pString[i] == (char)pChar)
+		{
+			return &(pString[i]);
+		}
+	}
+	return NULL;
+}
+
+int bcmp(const void *b1, const void *b2, size_t pLength)
+{
+	return memcmp(b1, b2, pLength);
+}
+
+float rint(float pNumber)
+{
+	float tDiff =  pNumber - ((int)(pNumber/1));
+
+	if (tDiff < 5)
+	{
+		return pNumber - tDiff;
+	}
+	else
+	{
+		return pNumber + (1 - tDiff);
+	}
+}
+
+int gettimeofday(struct timeval *tp, struct timezone *tzp)
+{
+	struct timeb tTime;
+	ftime(&tTime);
+	if (tp)
+	{
+		tp->tv_sec = tTime.time;
+		tp->tv_usec = tTime.millitm;
+	}
+	if (tzp)
+	{
+		tzp->tz_minuteswest = tTime.timezone;
+		tzp->tz_dsttime = tTime.dstflag;
+	}
+	return -1;
+}
+
+#endif
---
+++ bits/SpaceGroup/PCPort.h
@@ -0,0 +1,22 @@
+
+
+#if !defined(__PC_PORT_H__)
+#define __PC_PORT_H__
+int prog_main(int argc, const char * argv[]);	//The main function
+
+struct timeval{
+	long tv_sec;	/*seconds since Jan. 1, 1970*/
+	long tv_usec;
+};
+
+struct timezone{
+	int tz_minuteswest; /* of greenwich */
+	int tz_dsttime;		/* type of dst correction to apply*/
+};
+
+extern void bzero(void* pPoint, long pBytes);
+extern char* index(char* pString, int pChar);
+extern int bcmp(const void *b1, const void *b2, size_t pLength);
+extern float rint(float pNumber);
+extern int gettimeofday(struct timeval *tp, struct timezone *tzp);
+#endif
