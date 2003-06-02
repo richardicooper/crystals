167853afdd8f4c19a97fbdaf9b9f9f4bad48f4e6
richardicooper <richardiancooper@gmail.com>
2003-06-02 16:55:12 +0000

    Added code so that it was possible to select if a crystal is chiral. From either the command line of not. Added code so that it is possible to send a verbose flag and get the stats tables back. Added code for filtering test info. Removed come old comments.

--- bits/SpaceGroup/main.cpp
+++ bits/SpaceGroup/main.cpp
@@ -35,9 +35,7 @@
  * };
  */
  
-//#if defined(_WIN32)
 //#include "stdafx.h"
-//#endif
 #if defined(_WIN32)
 #include <direct.h>
 #endif
@@ -52,7 +50,7 @@
 #include "Stats.h"
 #include "UnitCell.h"
 
-#include "StringClassses.h"
+#include "StringClasses.h"
 
 using namespace std;
 
@@ -70,24 +68,22 @@ typedef struct RunStruct
 {
     Path iFileName;		//File name for the hkl data.
     Path iTablesFile;		//The file name of the tables file.
-    char* iOtherInfoFile;	//The name of a file for more informtion.
-    bool  iChiral;			//false is not nessaseraly chiral. false chiral
+    Path iOtherInfoFile;	//The name of a file for more informtion.
+    bool iChiral;		//False is not nessaseraly chiral. false chiral
+    bool iRequestChirality;	//If this is true then the user will be asked if the file crystal is chiral.
+    bool iVerbose;
     char* iCrystalSys;		//Crystal System
 }RunStruct;
 
 void runTest(RunStruct *pRunData)
 {
     std::cout << "Reading in tables...";
-    struct timeval time1;
+	struct timeval time1;
     struct timeval time2;
     gettimeofday(&time1, NULL);
     Tables* tTables = new Tables(pRunData->iTablesFile.getCString());
     gettimeofday(&time2, NULL);
     std::cout << "\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
-    if (pRunData->iCrystalSys[0] == 0)
-    {
-        strcpy(pRunData->iCrystalSys, getCrystalSystem());
-    }
     std::cout << "\nReading in hkl data...";
     gettimeofday(&time1, NULL);
     HKLData* tHKL = new HKLData(pRunData->iFileName.getCString());
@@ -104,17 +100,24 @@ void runTest(RunStruct *pRunData)
         tStats->addReflection(tReflection);
     }
     gettimeofday(&time2, NULL);
+    
     std::cout <<"\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
     gettimeofday(&time1, NULL);
     tStats->calProbs();
     gettimeofday(&time2, NULL);
+    if (pRunData->iVerbose)
+        std::cout << *tStats << "\n";
     std::cout <<"\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
     std::cout << "\nRanking space groups...";
     gettimeofday(&time1, NULL);
     RankedSpaceGroups* tRankings = new RankedSpaceGroups(*tTable, *tStats, pRunData->iChiral);
     gettimeofday(&time2, NULL);
     std::cout <<"\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
-    std::cout << "\n" << tRankings << "\n";
+    std::cout << "\n" << *tRankings << "\n";
+    if (tStats->filtered())
+    {
+        std::cout << "\nThis data may have already been filtered!\n";
+    }
     delete tRankings;
     delete tStats;
     delete tHKL;
@@ -131,15 +134,13 @@ void initRunStruct(RunStruct *pRunStruct)
 	char * tWorkingPath = NULL;
 	tWorkingPath = getcwd(NULL, PATH_MAX);
 #endif
-    //pRunStruct->iTablesFile = new char[strlen(tWorkingPath)+strlen(kDefaultTables)+1];
     pRunStruct->iTablesFile.initFormated("%s%s", tWorkingPath, kDefaultTables);
-    //sprintf(pRunStruct->iTablesFile, "%s%s", tWorkingPath, kDefaultTables);
     free(tWorkingPath);
-//    pRunStruct->iFileName = new char[PATH_MAX];
-//    pRunStruct->iFileName[0] = 0;
     pRunStruct->iCrystalSys = new char[20];
     pRunStruct->iCrystalSys[0] = 0;
+    pRunStruct->iRequestChirality = true;
     pRunStruct->iChiral = false;
+    pRunStruct->iVerbose = false;
 }
 
 bool handleArg(int *pPos, int pMax, _TCHAR * argv[], RunStruct *pRunData)
@@ -147,6 +148,20 @@ bool handleArg(int *pPos, int pMax, _TCHAR * argv[], RunStruct *pRunData)
     if (strcmp(argv[(*pPos)], "-c") == 0)
     {
         pRunData->iChiral = true;
+        pRunData->iRequestChirality = false;
+        (*pPos)++;
+        return true;
+    }
+    else if (strcmp(argv[(*pPos)], "-nc") == 0)
+    {
+        pRunData->iChiral = false;
+        pRunData->iRequestChirality = false;
+        (*pPos)++;
+        return true;
+    }
+    else if (strcmp(argv[*pPos], "-v")==0)
+    {
+        pRunData->iVerbose = true;
         (*pPos)++;
         return true;
     }
@@ -165,14 +180,21 @@ bool handleArg(int *pPos, int pMax, _TCHAR * argv[], RunStruct *pRunData)
         (*pPos)++;
         if (*pPos < pMax && argv[*pPos][0] != '-')
         {
-//            delete pRunData->iTablesFile;
-//            pRunData->iTablesFile = new char[strlen(argv[*pPos])+1];
-//            strcpy(pRunData->iTablesFile, argv[*pPos]);
             pRunData->iTablesFile.init(argv[*pPos]);
             (*pPos)++;
             return true;
         }
     }
+	else if (strcmp(argv[*pPos], "-e") == 0)
+	{
+		(*pPos)++;
+        if (*pPos < pMax && argv[*pPos][0] != '-')
+        {
+            pRunData->iOtherInfoFile.init(argv[*pPos]);
+            (*pPos)++;
+            return true;
+        }
+	}
     else if (strcmp(argv[*pPos], "-s")==0)
     {
         (*pPos)++;
@@ -200,11 +222,13 @@ bool handleArgs(int pArgc, _TCHAR* argv[], RunStruct *pRunData)
     {
         if (!handleArg(&tCount, pArgc, argv, pRunData))
         {
-            std::cout << "Usage: " << argv[0] << "[-f hklfile] [-t tablefile] [-c] [-s system]\n";
+            std::cout << "Usage: " << argv[0] << "[-f hklfile] [-t tablefile] [-e extradatafile] [-c|-nc] [-s system]\n";
             std::cout << "-f hklfile: The path of the hkl file to read in.\n";
             std::cout << "-t tablefile: The path of the table file.\n";
-            std::cout << "-c: The structure is chiral.\n";
+			std::cout << "-e extradatafile: The path to a file which contains other data such as the unit cell.\n";
+            std::cout << "[-c| -nc]: -c The structure is chiral. -nc The crystal isn't necessarily chiral.\n";
             std::cout << "-s system#: The crystal system table to use.\n";
+            std::cout << "-v: verbose output\n";
             std::cout << "Valid values for system are:\n" <<
             "\t0: Triclinic" <<
             "\t1: MonoclinicA\n" <<
@@ -222,6 +246,41 @@ bool handleArgs(int pArgc, _TCHAR* argv[], RunStruct *pRunData)
     return true;
 }
 
+void getParamsFromUser(RunStruct *pRunData)
+{
+    if (pRunData->iFileName.getCString()[0] == 0)
+    {
+        std::cout << "Enter hkl file path: ";
+        std::cin >> pRunData->iFileName;
+        pRunData->iFileName.removeOutterQuotes();
+    }
+    char tReply;
+    while (pRunData->iRequestChirality)
+    {
+        std::cout << "Is the crystal chiral? [y/n]";
+    	std::cin >> tReply;
+        if (tReply == 'N' || tReply == 'n')
+        {
+            pRunData->iChiral = false;
+            pRunData->iRequestChirality = false;
+        }
+        else if (tReply == 'Y' || tReply == 'y')
+        {
+            pRunData->iChiral = true;
+            pRunData->iRequestChirality = false;
+        }
+    }
+    if (pRunData->iCrystalSys[0] == 0)
+    {
+        if (pRunData->iOtherInfoFile.getCString()[0] != 0)
+        {
+                UnitCell tUnitCell(pRunData->iOtherInfoFile.getCString());
+                tUnitCell.guessCrystalSystem();
+        }
+        strcpy(pRunData->iCrystalSys, getCrystalSystem());
+    }
+}
+
 #if !defined(_WIN32)
 int main(int argc, const char * argv[])
 #else
@@ -235,12 +294,8 @@ int _tmain(int argc, _TCHAR* argv[])
     {
         return 0;
     }
-    if (tRunStruct.iFileName.getCString()[0] == 0)
-    {
-        std::cout << "Enter hkl file path: ";
-        std::cin >> tRunStruct.iFileName;
-        tRunStruct.iFileName.removeOutterQuotes();
-    }
+    getParamsFromUser(&tRunStruct);
+    
     try
     {
         runTest(&tRunStruct);
@@ -249,8 +304,6 @@ int _tmain(int argc, _TCHAR* argv[])
     {
         std::cout << "\n" << eE.what() << "\n";
     }   
-   // delete tRunStruct.iFileName;
-    //delete tRunStruct.iTablesFile;
     delete tRunStruct.iCrystalSys;
-	return 0;
+    return 0;
 }
