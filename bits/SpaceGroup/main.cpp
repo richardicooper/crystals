2877ebb79aa10981e3bfe5c50b2c5333d42265df
richardicooper <richardiancooper@gmail.com>
2003-06-10 15:05:40 +0000

    Added calls to the interactive mode and code to handle the new command line options. Added the code to handle the option to write the stats table and the ranks space groups to a file. Added some better exception handling. Started to add code to read in a batch parameter file.

--- bits/SpaceGroup/main.cpp
+++ bits/SpaceGroup/main.cpp
@@ -49,6 +49,7 @@
 #include "CrystalSystem.h"
 #include "Stats.h"
 #include "UnitCell.h"
+#include "InteractiveControl.h"
 
 #include "StringClasses.h"
 
@@ -69,24 +70,66 @@ typedef struct RunStruct
     Path iFileName;		//File name for the hkl data.
     Path iTablesFile;		//The file name of the tables file.
     Path iOtherInfoFile;	//The name of a file for more informtion.
+    Path iOutputFile;		//The path of the file where the stats and ranking should be output to.
+    Path iParamFile;		//A path to the file which contains any further parameters.
     bool iChiral;		//False is not nessaseraly chiral. false chiral
     bool iRequestChirality;	//If this is true then the user will be asked if the file crystal is chiral.
-    bool iVerbose;
+    bool iInteractiveMode;	//If this is set true the program will go into interactive mode at the end.
+    bool iVerbose;		//If this is set true the program will output the stats table after it has calculated all it's probabilits.
     char* iCrystalSys;		//Crystal System
 }RunStruct;
+
+void outputToFile(RunStruct* pRunData, Stats* pStats, RankedSpaceGroups* pRanking)	//This outputs the ranked spacegroups and the stats table to the file at the path pRunData->iOutputFile
+{
+    if (pRunData->iOutputFile.getCString()[0] != 0)
+    {
+        filebuf tOutputFile;
+        if (tOutputFile.open(pRunData->iOutputFile.getCString(), ios::out) != NULL)
+        {
+            ostream tOutputStream(&tOutputFile);
+            tOutputStream << *pStats << "\n";
+            tOutputStream << *pRanking << "\n";
+            tOutputFile.close();
+        }
+        else
+        {
+            FileException tException(errno);
+            tException.addError("When opening output file:");
+            throw tException;
+        }
+    }
+}
 
 void runTest(RunStruct *pRunData)
 {
     std::cout << "Reading in tables...";
-	struct timeval time1;
+    struct timeval time1;
     struct timeval time2;
     gettimeofday(&time1, NULL);
-    Tables* tTables = new Tables(pRunData->iTablesFile.getCString());
+    Tables* tTables;
+    try
+    {
+        tTables = new Tables(pRunData->iTablesFile.getCString());
+    }
+    catch (MyException& eException)
+    {
+        eException.addError("When opening table file:");
+        throw eException;
+    }
     gettimeofday(&time2, NULL);
     std::cout << "\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
     std::cout << "\nReading in hkl data...";
     gettimeofday(&time1, NULL);
-    HKLData* tHKL = new HKLData(pRunData->iFileName.getCString());
+    HKLData* tHKL;
+    try
+    {
+        tHKL = new HKLData(pRunData->iFileName.getCString());
+    }
+    catch (MyException& eException)
+    {
+        eException.addError("When opening hkl file:");
+        throw eException;
+    }
     gettimeofday(&time2, NULL);
     std::cout << "\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
     std::cout << "\nCalculating probabilities...";
@@ -99,14 +142,13 @@ void runTest(RunStruct *pRunData)
         Reflection* tReflection = tHKL->getReflection(i);
         tStats->addReflection(tReflection);
     }
-    gettimeofday(&time2, NULL);
-    
+    gettimeofday(&time2, NULL);    
     std::cout <<"\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
     gettimeofday(&time1, NULL);
     tStats->calProbs();
     gettimeofday(&time2, NULL);
     if (pRunData->iVerbose)
-        std::cout << *tStats << "\n";
+	std::cout << *tStats << "\n";
     std::cout <<"\n" << (float)(time2.tv_sec - time1.tv_sec)+(float)(time2.tv_usec-time1.tv_usec)/1000000 << "s\n";
     std::cout << "\nRanking space groups...";
     gettimeofday(&time1, NULL);
@@ -117,6 +159,12 @@ void runTest(RunStruct *pRunData)
     if (tStats->filtered())
     {
         std::cout << "\nThis data may have already been filtered!\n";
+    }
+    outputToFile(pRunData, tStats, tRankings);
+    if (pRunData->iInteractiveMode)
+    {
+        InteractiveControl tInteractiveControl(tStats, tRankings, tTable);
+        tInteractiveControl.goInteractive();
     }
     delete tRankings;
     delete tStats;
@@ -141,6 +189,7 @@ void initRunStruct(RunStruct *pRunStruct)
     pRunStruct->iRequestChirality = true;
     pRunStruct->iChiral = false;
     pRunStruct->iVerbose = false;
+    pRunStruct->iInteractiveMode = false;
 }
 
 bool handleArg(int *pPos, int pMax, _TCHAR * argv[], RunStruct *pRunData)
@@ -165,12 +214,28 @@ bool handleArg(int *pPos, int pMax, _TCHAR * argv[], RunStruct *pRunData)
         (*pPos)++;
         return true;
     }
+    else if (strcmp(argv[*pPos], "-i")==0)
+    {
+        pRunData->iInteractiveMode = true;
+        (*pPos)++;
+        return true;
+    }
     else if (strcmp(argv[*pPos], "-f")==0)
     {
         (*pPos)++;
         if (*pPos < pMax && argv[*pPos][0] != '-')
         {
             pRunData->iFileName.init(argv[*pPos]);
+            (*pPos)++;
+            return true;
+        }
+    }
+    else if (strcmp(argv[*pPos], "-o")==0)	//Output to file.
+    {
+        (*pPos)++;
+        if (*pPos < pMax && argv[*pPos][0] != '-')
+        {
+            pRunData->iOutputFile.init(argv[*pPos]);
             (*pPos)++;
             return true;
         }
@@ -185,16 +250,26 @@ bool handleArg(int *pPos, int pMax, _TCHAR * argv[], RunStruct *pRunData)
             return true;
         }
     }
-	else if (strcmp(argv[*pPos], "-e") == 0)
-	{
-		(*pPos)++;
+    else if (strcmp(argv[*pPos], "-b")==0)
+    {
+        (*pPos)++;
+        if (*pPos < pMax && argv[*pPos][0] != '-')
+        {
+            pRunData->iParamFile.init(argv[*pPos]);
+            (*pPos)++;
+            return true;
+        }
+    }
+    else if (strcmp(argv[*pPos], "-e") == 0)
+    {
+        (*pPos)++;
         if (*pPos < pMax && argv[*pPos][0] != '-')
         {
             pRunData->iOtherInfoFile.init(argv[*pPos]);
             (*pPos)++;
             return true;
         }
-	}
+    }
     else if (strcmp(argv[*pPos], "-s")==0)
     {
         (*pPos)++;
@@ -215,6 +290,11 @@ bool handleArg(int *pPos, int pMax, _TCHAR * argv[], RunStruct *pRunData)
     return false;
 }
 
+void readParamFile(RunStruct *pRunData)
+{
+    
+}
+
 bool handleArgs(int pArgc, _TCHAR* argv[], RunStruct *pRunData)
 {
     int tCount = 1;
@@ -225,12 +305,15 @@ bool handleArgs(int pArgc, _TCHAR* argv[], RunStruct *pRunData)
             std::cout << "Usage: " << argv[0] << "[-f hklfile] [-t tablefile] [-e extradatafile] [-c|-nc] [-s system]\n";
             std::cout << "-f hklfile: The path of the hkl file to read in.\n";
             std::cout << "-t tablefile: The path of the table file.\n";
-			std::cout << "-e extradatafile: The path to a file which contains other data such as the unit cell.\n";
+            std::cout << "-e extradatafile: The path to a file which contains other data such as the unit cell.\n";
+            std::cout << "-o outputfile: The path to a file to output the stats table and the raking table.\n";
             std::cout << "[-c| -nc]: -c The structure is chiral. -nc The crystal isn't necessarily chiral.\n";
-            std::cout << "-s system#: The crystal system table to use.\n";
             std::cout << "-v: verbose output\n";
+            std::cout << "-i: Interactive mode. This allows the user to veiw the stats table.\n";
+            std::cout << "-s system#: The crystal system table to use.\n";
+            std::cout << "-b batchfile: This supplies a file with all the parameters needed to run the program. The parameters in the file override any other parameters set on the command line.";
             std::cout << "Valid values for system are:\n" <<
-            "\t0: Triclinic" <<
+            "\t0: Triclinic\n" <<
             "\t1: MonoclinicA\n" <<
             "\t2: MonoclinicB\n" <<
             "\t3: MonoclinicC\n" <<
@@ -243,6 +326,10 @@ bool handleArgs(int pArgc, _TCHAR* argv[], RunStruct *pRunData)
             return false;
         }
     }
+    if (pRunData->iParamFile.getCString()[0] != 0)
+    {
+        readParamFile(pRunData);
+    }
     return true;
 }
 
@@ -250,21 +337,26 @@ void getParamsFromUser(RunStruct *pRunData)
 {
     if (pRunData->iFileName.getCString()[0] == 0)
     {
+        char tString[255];
         std::cout << "Enter hkl file path: ";
-        std::cin >> pRunData->iFileName;
+        std::cin >> tString;
+        
+        pRunData->iFileName.init(tString);
         pRunData->iFileName.removeOutterQuotes();
     }
-    char tReply;
+    char tReply[10];
     while (pRunData->iRequestChirality)
     {
         std::cout << "Is the crystal chiral? [y/n]";
-    	std::cin >> tReply;
-        if (tReply == 'N' || tReply == 'n')
+        std::cin.getline(tReply, 10);
+        String::upcase(tReply);
+        if (strcmp(tReply, "NO")  == 0 || strcmp(tReply, "N") == 0 || 0 == tReply[0])
         {
+            cout << "No" << "\n";
             pRunData->iChiral = false;
             pRunData->iRequestChirality = false;
         }
-        else if (tReply == 'Y' || tReply == 'y')
+        else if (strcmp(tReply, "YES") == 0 || strcmp(tReply, "Y") == 0)
         {
             pRunData->iChiral = true;
             pRunData->iRequestChirality = false;
@@ -287,6 +379,10 @@ int main(int argc, const char * argv[])
 int _tmain(int argc, _TCHAR* argv[])
 #endif
 {
+    String myString;
+    cout << "--> ";
+    std::cin >> myString;
+    cout << myString << "\n";
     std::cout << argv[0] << "\n";   
     RunStruct tRunStruct;
     initRunStruct(&tRunStruct);
