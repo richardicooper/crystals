fb23bbc099828cf5870d69ccb9cd2bf0d155ea5e
richardicooper <richardiancooper@gmail.com>
2003-06-10 15:12:24 +0000

    Just added.

---
+++ bits/SpaceGroup/RunParameters.cpp
@@ -0,0 +1,11 @@
+/*
+ *  RunParameters.cpp
+ *  Space Groups
+ *
+ *  Created by Stefan on Tue Jun 10 2003.
+ *  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "RunParameters.h"
+
---
+++ bits/SpaceGroup/RunParameters.h
@@ -0,0 +1,11 @@
+/*
+ *  RunParameters.h
+ *  Space Groups
+ *
+ *  Created by Stefan on Tue Jun 10 2003.
+ *  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+
+
