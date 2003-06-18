43416a0a313dc62155e435ba4a1564b9e2cb4fca
richardicooper <richardiancooper@gmail.com>
2003-06-18 16:19:45 +0000

    Removed the output which will output the who table.

--- bits/SpaceGroup/Stats.h
+++ bits/SpaceGroup/Stats.h
@@ -34,9 +34,7 @@ class Stats:public MyObject
         float iTotalIntensity;
         ElemStats* iStats;	//Cells of the stats.
         bool iFiltered;
-        void outputHeadings(std::ostream& pStream);
         void outputHeadings(std::ostream& pStream, signed char pColumnsToPrint[], int pNumOfColums);
-        void outputRow(int pRow, std::ostream& pStream);
         void outputRow(int pRow, std::ostream& pStream, signed char pColumnsToPrint[], int pNumOfColums);
         static float evaluationFunction(float pX, float AbsentM, float AbsentSD, float PresentM, float PresentSD);
         void handleFilteredData(int pColumns[], int pNumColumns);
@@ -48,10 +46,7 @@ class Stats:public MyObject
         void addReflection(Reflection* tReflection);
         void calProbs();			//Calculates all the probabilites for all the cells.
         ElemStats* getElem(int pHeadIndex, int pCondIndex);
-        std::ostream& output(std::ofstream& pStream, Table& pTable);
-        std::ostream& output(std::ostream& pStream);
+        std::ostream& output(std::ostream& pStream, Table& pTable);
 };
-
-std::ostream& operator<<(std::ostream& pStream, Stats& pStats);
 
 #endif
