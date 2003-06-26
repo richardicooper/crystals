8bc14ab9683cb43e1f86f8c8b8b381e95233fd3c
richardicooper <richardiancooper@gmail.com>
2003-06-26 14:21:48 +0000

    Added the iterator interface because it's a good a place as any.

--- bits/SpaceGroup/BaseTypes.h
+++ bits/SpaceGroup/BaseTypes.h
@@ -5,7 +5,10 @@
  *  Created by Stefan on Wed Jun 25 2003.
  *  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
  *
+ *
+ *   This file is for putting file types which need to be extended to be of any use.
  */
+ 
 #if !defined(__BASE_TYPES_H__)
 #define __BASE_TYPES_H__
 
@@ -18,4 +21,26 @@ class Column:public MyObject
         int length();
 };
 
+template <class obj>
+class Iterator:public MyObject
+{
+public:
+    virtual ~Iterator()
+    {
+    }
+
+    virtual void reset()
+    {
+    }
+    
+    virtual obj* next()
+    {
+        return NULL;
+    }
+    
+    virtual bool hasNext()
+    {
+        return false;
+    }
+};
 #endif
