282bf5c3776487224bac1dd8d8009282a4e0b2bc
richardicooper <richardiancooper@gmail.com>
2003-09-23 13:39:23 +0000

    1. Removed the Condition and Conditions class definitions from the file. 2. Made a couple of parameters consts

--- bits/SpaceGroup/CrystalSystem.cpp
+++ bits/SpaceGroup/CrystalSystem.cpp
@@ -50,7 +50,6 @@
 using namespace std;
 
 static regex_t* iHeaderRE  =NULL;
-static regex_t* iCondRE  =NULL;
 static regex_t* iFirstNumRE  =NULL;
 static regex_t* iFirstNSpRE  =NULL;
 static regex_t* iTableRE  =NULL;
@@ -62,8 +61,6 @@ void initCrysRegEx()
     {
         char tHeaderRE[] = "^([[:digit:]]+)\t([hkl0-]{3}[hkl0-]?)\t(\\[[0123456789;. -]+\\])[[:space:]]*$";
             //Checks the format of the header line.
-        char tCondRE[] = "^([[:digit:]]+)\t(([[:alnum:]]|[- =+])+)\t(\\[( *([-+]?[[:digit:]]+(\\.[[:digit:]]+)?) *(;)?)*( *([-+]?[[:digit:]]+(\\.[[:digit:]]+)?) *)\\])\t([[:digit:]]+)[[:space:]]*$";
-            //Checks the formoat of a line in the conditons list is the conrrect format.
         char tFirstNumRE[] = "([[:digit:]]),?[[:space:]]?"; 
             //Takes the first number of a , delimited list of numbers.
         char tFirstNSpRE[] = "([^\t]+)(\t([^\t].+))?";	
@@ -73,13 +70,12 @@ void initCrysRegEx()
         char tTableArgsRE[] = "([^\t]+)\t([[:digit:]]+), ([[:digit:]]+)";
             //The header line of the table. This contains the name of the system and the number of condition columns and the number of point group columns.
         iHeaderRE  = new regex_t;
-        iCondRE  = new regex_t;
+        
         iFirstNumRE  = new regex_t;
         iFirstNSpRE  = new regex_t;
         iTableRE  = new regex_t;
         iTableArgsRE = new regex_t;
         regcomp(iHeaderRE, tHeaderRE, REG_EXTENDED);
-        regcomp(iCondRE, tCondRE, REG_EXTENDED);
         regcomp(iFirstNumRE, tFirstNumRE, REG_EXTENDED);
         regcomp(iFirstNSpRE, tFirstNSpRE, REG_EXTENDED);
         regcomp(iTableRE, tTableRE, REG_EXTENDED);
@@ -92,13 +88,11 @@ void deinitRE()
     if (iHeaderRE != NULL)
     {
         delete iHeaderRE;
-        delete iCondRE;
         delete iFirstNumRE;
         delete iFirstNSpRE;
         delete iTableRE;
         delete iTableArgsRE;
         iHeaderRE = NULL;
-        iCondRE = NULL;
         iFirstNumRE = NULL;
         iFirstNSpRE = NULL;
         iTableRE = NULL;
@@ -163,213 +157,6 @@ std::ostream& operator<<(std::ostream& pStream, Heading& pHeader)
 {
     return pHeader.output(pStream);
 }
-
-Condition::Condition(char* pLine)
-{
-/* Match Index 	Description
-     * 0 	 	Line
-     * 1		Index of string
-     * 2 		Name of condition
-     * 3		Vector for condition
-     * 4		Bits and peaces
-     * 5		"
-     * 6		"
-     * 7		"
-     * 8		"
-     * 9		Multiplier for condition
-     */
-    const size_t tMatches = 15;
-    regmatch_t tMatch[tMatches];
-    
-    bzero(tMatch, sizeof(regmatch_t)*tMatches);
-    if (regexec(iCondRE, pLine, tMatches, tMatch, 0))
-    {
-        throw MyException(kUnknownException, "Condition had an invalid format!");
-    }
-    char* tString = new char[(int)(tMatch[4].rm_eo-tMatch[4].rm_so+1)];	//Get the matrix for the condition
-    tString[(int)(tMatch[4].rm_eo-tMatch[4].rm_so)] = 0;
-    strncpy(tString, pLine+(int)tMatch[4].rm_so, (int)tMatch[4].rm_eo-tMatch[4].rm_so);  
-    iMatrix = new MatrixReader(tString);
-    delete[] tString;
-    iName = new char[(int)(tMatch[2].rm_eo-tMatch[2].rm_so+1)];		//Get the name of the condition
-    iName[(int)(tMatch[2].rm_eo-tMatch[2].rm_so)] = 0;
-    strncpy(iName, pLine+(int)tMatch[2].rm_so, (int)tMatch[2].rm_eo-tMatch[2].rm_so);  
-    iID = strtol((const char*)(pLine+tMatch[1].rm_so), NULL, 10);			//Get the id of the condition
-    iMult = (float)strtod((const char*)(pLine+tMatch[12].rm_so), NULL);			//Get the multiplier 
-}
-
-Condition::~Condition()
-{
-    delete[] iName;
-    delete iMatrix;
-}
-
-char* Condition::getName()
-{
-    return iName;
-}
-
-Matrix<short>* Condition::getMatrix()
-{
-    return iMatrix;
-}
-
-float Condition::getMult()
-{
-    return iMult;
-}
-        
-int Condition::getID()
-{
-    return iID;
-}
-
-std::ostream& Condition::output(std::ostream& pStream)
-{
-    pStream << iID << "\t" << iName << "\t" << *iMatrix << " " << iMult << "\n";
-    return pStream;
-}
-
-std::ostream& operator<<(std::ostream& pStream, Condition& pCondition)
-{
-    return pCondition.output(pStream);
-}
-
-Conditions::Conditions()
-{
-    iConditions = new ArrayList<Condition>(1);
-}
-
-Conditions::~Conditions()
-{
-    int iSize = iConditions->length();
-    for (int i = 0; i < iSize; i++)
-    {
-        Condition* tCondition = iConditions->remove(i);
-        if (tCondition)
-        {
-            delete tCondition;
-        }
-    }
-    delete iConditions;
-}
-
-char* Conditions::getName(int pIndex)
-{
-    Condition* tCondition = iConditions->get(pIndex);
-    
-    if (!tCondition)
-    {
-        throw MyException(kUnknownException, "Condition with that ID doesn't exist.");
-    }
-    return tCondition->getName();
-}
-
-Matrix<short>* Conditions::getMatrix(int pIndex)
-{
-    Condition* tCondition = iConditions->get(pIndex);
-    
-    if (!tCondition)
-    {
-        throw MyException(kUnknownException, "Condition with that ID doesn't exist.");
-    }
-    return tCondition->getMatrix();
-}
-
-float Conditions::getMult(int pIndex)
-{
-    Condition* tCondition = iConditions->get(pIndex);
-    
-    if (!tCondition)
-    {
-        throw MyException(kUnknownException, "Condition with that ID doesn't exist.");
-    }
-    return tCondition->getMult();
-}
-
-char* Conditions::addCondition(char* pLine)	//Returns the point which the line at the start of this string finishs
-{
-    char* tNext = strchr(pLine, '\n');
-    Condition* tCondition;
-    if (tNext == NULL)
-    {
-           tCondition = new Condition(pLine);
-    }
-    else
-    {
-        char *tString = new char[(int)(tNext-pLine)+1];
-        tString[(int)(tNext-pLine)] = 0;
-        strncpy(tString, pLine, (int)(tNext-pLine));
-        tCondition = new Condition(tString);
-        tNext++;
-        delete[] tString;
-    }
-    iConditions->setWithAdd(tCondition, tCondition->getID());
-    return tNext;
-}
-
-void Conditions::readFrom(filebuf& pFile)
-{
-    char tHeaderLine[] = "ID	Name	Vector	Multiplier";
-	std::istream tFile(&pFile);
-    char tLine[255];
-    int tLineNum = 0;
-    
-    do
-    {
-        tFile.getline(tLine, 255);
-        String::trim(tLine);
-    }while (strstr(tLine, tHeaderLine) == NULL && !tFile.eof());
-    do
-    {
-        tFile.getline(tLine, 255);
-        String::trim(tLine);
-        try
-        {
-            if (tLine[0] != '\0')
-                    addCondition(tLine);
-        }
-        catch (MyException& eE)
-        {
-            char tText[255];
-            sprintf(tText, "On line %d", tLineNum);
-            eE.addError(tText);
-            throw eE;
-        }
-        tLineNum++;
-    }
-    while (!tFile.eof() && strlen(tLine)>0);
-}
-
-int Conditions::length()
-{
-    return iConditions->length();
-}
-
-std::ostream& Conditions::output(std::ostream& pStream)
-{
-    int tSize = iConditions->length();
-    for (int i = 0; i < tSize; i++)
-    {
-        Condition* tCondition = iConditions->get(i);
-        if (tCondition)
-        {
-            std::cout << *tCondition << "\n";
-        }
-        else
-        {
-            std::cout << "null\n";
-        }
-    }
-    return pStream;
-}
-
-
-std::ostream& operator<<(std::ostream& pStream, Conditions& pConditions)
-{
-    return pConditions.output(pStream);
-}
-
 
 Headings::Headings()
 {
@@ -502,123 +289,6 @@ std::ostream& operator<<(std::ostream& pStream, Headings& pHeaders)
     return pHeaders.output(pStream);
 }
 
-Index::Index(signed char pValue)
-{
-    iValue = pValue;
-}
-        
-Index::Index(Index& pObject)
-{
-    *this = pObject;
-}
-
-signed char Index::get()
-{
-    return iValue;
-}
-
-void Index::set(signed char pValue)
-{
-    iValue = pValue;
-}
-
-Index& Index::operator=(Index& pObject)
-{
-    iValue = pObject.iValue;
-    return *this;
-}
-
-bool Index::operator<(Index& pObject)
-{
-    return iValue < pObject.iValue;
-}
-
-bool Index::operator>(Index& pObject)
-{
-    return iValue > pObject.iValue;
-}
-
-bool Index::operator==(Index& pObject)
-{
-    return iValue == pObject.iValue;
-}
-
-std::ostream& Index::output(std::ostream& pStream)
-{
-    return pStream << (int)iValue;
-}
-
-std::ostream& operator<<(std::ostream& pStream, Index& pIndex)
-{
-    return pIndex.output(pStream);
-}
-
-Indexs::Indexs(signed char pIndex)
-{
-    iIndexs = new ArrayList<Index>(1);
-    iIndexs->add(new Index(pIndex));
-}
-
-void Indexs::addIndex(signed char pIndex)
-{
-    iIndexs->add(new Index(pIndex));
-}
-
-int Indexs::number()
-{
-    return iIndexs->length();
-}
-
-Index* Indexs::getIndex(int pIndex)
-{
-    return iIndexs->get(pIndex);
-}
-
-signed char Indexs::getValue(int pIndex)
-{
-    return iIndexs->get(pIndex)->get();
-}
-
-Indexs::~Indexs()
-{
-    int tCount = iIndexs->length();
-    for (int i = 0; i < tCount;  i++)
-    {
-        Index* tTemp = iIndexs->remove(i);
-        if (tTemp)
-        {
-            delete tTemp;
-        }
-    }
-    delete iIndexs;
-}
-
-std::ostream& Indexs::output(std::ostream& pStream)
-{
-    int tCount = iIndexs->length();
-    char* tString = new char[tCount*2+3];
-    tString[0] = '\0';
-    for (int i = 0; i < tCount;  i++)
-    {
-        if (i+1==tCount)
-        {
-            sprintf(tString, "%s%d", tString, iIndexs->get(i)->get());
-        }
-        else
-        {
-            sprintf(tString, "%s%d ", tString, iIndexs->get(i)->get());
-        }
-    }
-    pStream << tString;
-    delete tString;
-    return pStream;
-}
-
-std::ostream& operator<<(std::ostream& pStream, Indexs& pIndexs)
-{
-    return pIndexs.output(pStream);
-}
-        
 ConditionColumn::ConditionColumn()
 {
     iHeadingConditions = new ArrayList<Index>(1);
@@ -744,6 +414,123 @@ std::ostream& ConditionColumn::output(std::ostream& pStream, Headings* pHeadings
         pStream << "\n";
     }
     return pStream;
+}
+
+Index::Index(signed char pValue)
+{
+    iValue = pValue;
+}
+        
+Index::Index(Index& pObject)
+{
+    *this = pObject;
+}
+
+signed char Index::get()
+{
+    return iValue;
+}
+
+void Index::set(signed char pValue)
+{
+    iValue = pValue;
+}
+
+Index& Index::operator=(Index& pObject)
+{
+    iValue = pObject.iValue;
+    return *this;
+}
+
+bool Index::operator<(Index& pObject)
+{
+    return iValue < pObject.iValue;
+}
+
+bool Index::operator>(Index& pObject)
+{
+    return iValue > pObject.iValue;
+}
+
+bool Index::operator==(Index& pObject)
+{
+    return iValue == pObject.iValue;
+}
+
+std::ostream& Index::output(std::ostream& pStream)
+{
+    return pStream << (int)iValue;
+}
+
+std::ostream& operator<<(std::ostream& pStream, Index& pIndex)
+{
+    return pIndex.output(pStream);
+}
+
+Indexs::Indexs(signed char pIndex)
+{
+    iIndexs = new ArrayList<Index>(1);
+    iIndexs->add(new Index(pIndex));
+}
+
+void Indexs::addIndex(signed char pIndex)
+{
+    iIndexs->add(new Index(pIndex));
+}
+
+int Indexs::number()
+{
+    return iIndexs->length();
+}
+
+Index* Indexs::getIndex(int pIndex)
+{
+    return iIndexs->get(pIndex);
+}
+
+signed char Indexs::getValue(int pIndex)
+{
+    return iIndexs->get(pIndex)->get();
+}
+
+Indexs::~Indexs()
+{
+    int tCount = iIndexs->length();
+    for (int i = 0; i < tCount;  i++)
+    {
+        Index* tTemp = iIndexs->remove(i);
+        if (tTemp)
+        {
+            delete tTemp;
+        }
+    }
+    delete iIndexs;
+}
+
+std::ostream& Indexs::output(std::ostream& pStream)
+{
+    int tCount = iIndexs->length();
+    char* tString = new char[tCount*2+3];
+    tString[0] = '\0';
+    for (int i = 0; i < tCount;  i++)
+    {
+        if (i+1==tCount)
+        {
+            sprintf(tString, "%s%d", tString, iIndexs->get(i)->get());
+        }
+        else
+        {
+            sprintf(tString, "%s%d ", tString, iIndexs->get(i)->get());
+        }
+    }
+    pStream << tString;
+    delete tString;
+    return pStream;
+}
+
+std::ostream& operator<<(std::ostream& pStream, Indexs& pIndexs)
+{
+    return pIndexs.output(pStream);
 }
 
 Table::Table(char* pName, Headings* pHeadings, Conditions* pConditions, int pNumColumns, int pNumPointGroups)
@@ -1151,7 +938,7 @@ int Table::chiralPointGroups(int pPointGroupIndeces[])
 	return tIndecesPoint;
 }
 
-int Table::dataUsed(signed char pIndices[], int pMax)
+int Table::dataUsed(signed char pIndices[], const int pMax) const
 {
     int tNumColumns = iColumns->length();
     TreeSet<signed char> tIndices;
@@ -1182,7 +969,7 @@ int Table::dataUsed(signed char pIndices[], int pMax)
     return j; 
 }
 
-int Table::conditionsUsed(signed char pIndices[], int pMax)
+int Table::conditionsUsed(signed char pIndices[], const int pMax) const
 {
     int tNumColumns = iColumns->length();
     TreeSet<signed char> tIndices;
