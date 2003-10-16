da1799e597876c3a5b7d2b9b60efa87cca199c38
richardicooper <richardiancooper@gmail.com>
2003-10-16 16:24:04 +0000

    1. Added a copy constructer prototype to MergedData. 2. Change the the matrix indices for the constructer of MergeData from a vector to and Array to make it easier to use it with my math functions. 3. Added the prototype of LaueGroups. This class stored all the indices for the matrices for each laue group. These indices are the indices are for an internal data structure.

--- bits/SpaceGroup/ReflectionMerging.h
+++ bits/SpaceGroup/ReflectionMerging.h
@@ -15,27 +15,51 @@ using namespace std;
 
 struct lsreflection
 {
-    bool operator()(const Reflection* pReflection1, const Reflection* pReflection2) const
-    {
-        Matrix<short>* tHKL1 = pReflection1->getHKL();
-        Matrix<short>* tHKL2 = pReflection2->getHKL();
-        int tValue = tHKL1->bytecmp(*tHKL2);
-        if (tValue > 0)
-        {
-            return true;
-        }
-        return false;
-    }
+  bool operator()(const Reflection* pReflection1, const Reflection* pReflection2) const
+  {
+      Matrix<short>* tHKL1 = pReflection1->getHKL();
+      Matrix<short>* tHKL2 = pReflection2->getHKL();
+      int tValue = tHKL1->bytecmp(*tHKL2);
+      if (tValue > 0)
+      {
+	  return true;
+      }
+      return false;
+  }
 };
 
 class MergedData
 {
-    private:
-        void equivalentise(const Reflection& pReflection, vector<Matrix<short>*>& pMatrice, Reflection* pResult);
-        float iRFactor;
-    public:
-        MergedData(const HKLData& pData, vector<Matrix<short>*>& pMatrice);
-        std::ostream& output(std::ostream& pStream);
+ private:
+    void equivalentise(const Reflection& pReflection, Array<unsigned short>& pMatIndices, Reflection* pResult)const; 
+    float iRFactor;
+ public:
+    MergedData(const HKLData& pData, Array<unsigned short>& pMatIndices);
+    MergedData(const MergedData& pMergedData);//Copy constructure;
+    std::ostream& output(std::ostream& pStream);
 };
 
 std::ostream& operator<<(std::ostream& pStream, MergedData& tData);
+
+#define SystemRef unsigned int
+
+class LaueGroups
+{
+private:
+#define kTriclinicID 1
+#define kMonoclinicBID 2
+#define kOrtharombicID 3
+#define kTetragonalID 4
+#define kTrigonalID 5
+#define kHexagonalID 6
+#define kCubicID 7
+    
+    class LaueGroup;
+    Array<LaueGroup*>* iGroups;
+ public:
+    LaueGroups();
+    unsigned int getSystemRef(unsigned short* pNumGroup, const unsigned short pSystemID) const; //Returns a reference to the first system LaueGroup and the number of groups for that system in pNumGroups
+    MergedData mergeSystemGroup(const HKLData& pHKLData, const unsigned short pSystem, const unsigned short pGroup) const;
+    Array<unsigned short>& getGroupIndice(const unsigned int pSystemRef, const unsigned short pGroupNum) const;
+    ~LaueGroups();
+};
