f061a722db2d8473d234e449fa6c64596219547c
richardicooper <richardiancooper@gmail.com>
2003-10-17 10:13:00 +0000

    1. Added a copy constructer to MergedData. 2. Change the the matrix indices for the constructer of MergeData from a vector to and Array to make it easier to use it with my math functions this goes thought the code. 3. Made the laueClassMatrices store the matrices in a Array instead of a C array. This is to make it easier to delete the memory and I don't have to store the number of matrices my self. 4. Corrected the matrices. They shouldn't have had ,'s in them. 5. Wrote the getMatrix method. 6. Released the memory allocated for laueClassMatrices. iMatrices. 7. Wrote the class LaueGroups:LaueGroup. Which stores the indices of the metrices of a Laue group and the crystal system id which the laue group is a member of. 8. Wrote the laueGroups code. Constructing all the laueGroup s inside it sorting them in the Array. 9. getSystemRef will return a reference of the first lauegroup class for a system and the number of groups for that system. 10. mergeSystemGroup creates a mergedData object and merges the HKL values for the system it was passed. 11. Changed MergedData to use an Array of matrix indices instead of a vector containing matrices. This was so it wasn't necessary to copy all the matrices needed for a merge into a new array. 12. The division to get the final r-factor was the wrong way up. 13. Removed the 100* because it make no difference to the comparison. 14. Changed equivalentise so that it can use a Array of matrix indices. 15. Added several comments.

--- bits/SpaceGroup/ReflectionMerging.cpp
+++ bits/SpaceGroup/ReflectionMerging.cpp
@@ -8,177 +8,197 @@
  */
 
 #include "ReflectionMerging.h"
-#include <Math.h>
+#include "MathFunctions.h"
+#include "Collections.h"
+#include <math.h>
 #include <iostream>
 #include <iterator.h>
-#include "MathFunctions.h"
 
-/*Matrices
-Triclinic
-[1, 0, 0]
-[0, 1, 0]
-[0, 0, 1]
-
-MonoclinicA
-2/m 1 1
-[1, 0, 0]
-[0, -1, 0]
-[0, 0, -1]
-
-MonoclinicB
-1 2/m 1
-[-1, 0, 0]
-[0, 1, 0]
-[0, 0, -1]
-
-MonoclinicC
-1 1 2/m
-[-1, 0, 0]
-[0, -1, 0]
-[0, 0, 1]
-
-Ortharombic
-2/m 2/m 2/m
-[-1, 0, 0]	[1, 0, 0]	[-1, 0, 0]
-[0, 1, 0]	[0, -1, 0]	[0, -1, 0]
-[0, 0, 1]	[0, 0, 1]	[0, 0, 1]
-
-Tetragonal
-4/m
-[0, -1, 0]	[-1, 0, 0]	[0, 1, 0]
-[1, 0, 0]	[0, -1, 0]	[-1, 0, 0]
-[0, 0, -1]	[0, 0, 1]	[0, 0, 1]
-4/m m m
-[0, -1, 0]	[-1, 0, 0]	[-1, 0, 0] 	[0, 1, 0]	[0, 1, 0]	[1, 0, 0]
-[1, 0, 0]	[0, -1, 0]	[0, 1, 0]	[-1, 0, 0]	[1, 0, 0]	[0, -1, 0]
-[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]
-
-[0, -1, 0]
-[-1, 0, 0]
-[0, 0, 1]
-
-Trigonal
--3
-[0, -1, 0]	[-1, 1, 0]
-[1, -1, 0]	[-1, 0, 0]
-[0, 0, 1]	[0, 0, 1]
-
--3 m 1
-[0, -1, 0]	[-1, 1, 0]	[-1, 1, 0]	[1, 0, 0]	[0, -1, 0]
-[1, -1, 0]	[-1, 0, 0]	[0, 1, 0]	[1, -1, 0]	[-1, 0, 0]
-[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]
-
--3 1 m
-[0, -1, 0]	[-1, 1, 0]	[0, 1, 0]	[1, -1, 0]	[-1, 0, 0]
-[1, -1, 0]	[-1, 0, 0]	[1, 0, 0]	[0, -1, 0]	[-1, 1, 0]
-[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]
-
-Hexagonal
-6/m
-[1, -1, 0]	[0, -1, 0]	[-1, 0, 0]	[-1, 1, 0]	[0, 1, 0]
-[1, 0, 0]	[1, -1, 0]	[0, -1, 0]	[-1, 1, 0]	[-1, 1, 0]
-[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]
-
-6/m m m
-[1, -1, 0]	[0, -1, 0]	[-1, 0, 0]	[-1, 1, 0]	[0, 1, 0]	[-1, 1, 0]
-[1, 0, 0]	[1, -1, 0]	[0, -1, 0]	[-1, 0, 0]	[-1, 1, 0]	[0, 1, 0]
-[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]
-
-[0, 1, 0]	[1, 0, 0]	[1, -1, 0]	[0, -1, 0]	[-1, 0, 0]
-[1, 0, 0]	[1, -1, 0]	[0, -1, 0]	[-1, 0, 0]	[-1, 1, 0]
-[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]	[0, 0, 1]
-
-Cubic
-m -3
-[0, 1, 0]	[0, 0, 1]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[0, 1, 0]
-[0, 0, 1]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[1, 0, 0]	[0, 0, -1]
-[1, 0, 0]	[0, 1, 0]	[0, 0, -1]	[-1, 0, 0]	[0, -1, 0]	[-1, 0, 0]
-
-[0, 0, 1]	[0, 0, -1]	[-1, 0, 0]	[0, -1, 0]	[1, 0, 0]
-[-1, 0, 0]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[0, -1, 0]
-[0, -1, 0]	[0, -1, 0]	[0, 0, -1]	[-1, 0, 0]	[0, 0, -1]
-
-m -3 m
-[0, 1, 0]	[0, 0, 1]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[0, 1, 0]
-[0, 0, 1]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[1, 0, 0]	[0, 0, -1]
-[1, 0, 0]	[0, 1, 0]	[0, 0, -1]	[-1, 0, 0]	[0, -1, 0]	[-1, 0, 0]
-
-[0, 0, 1]	[0, 0, -1]	[-1, 0, 0]	[0, -1, 0]	[1, 0, 0]	[0, 1, 0]
-[-1, 0, 0]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[0, -1, 0]	[1, 0, 0]
-[0, -1, 0]	[0, -1, 0]	[0, 0, -1]	[-1, 0, 0]	[0, 0, -1]	[0, 0, 1]
-
-[0, 0, 1]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[1, 0, 0]	[0, 0, -1]
-[0, 1, 0]	[0, 0, 1]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[0, 1, 0]
-[1, 0, 0]	[0, 1, 0]	[0, 0, -1]	[-1, 0, 0]	[0, -1, 0]	[-1, 0, 0]
-
-[-1, 0, 0]	[1, 0, 0]	[0, 1, 0]	[0, 0, 1]	[0, -1, 0]
-[0, 0, 1]	[0, 0, -1]	[-1, 0, 0]	[0, -1, 0]	[1, 0, 0]
-[0, -1, 0]	[0, -1, 0]	[0, 0, -1]	[-1, 0, 0]	[0, 0, -1]
-*/
+/*
+ * A class which simply stores a list of all the matrices.
+ */
 
 class LaueClassMatrices:public MyObject
 {
-    private:
-        MatrixReader** iMatrices;
-    public:
-        LaueClassMatrices();
-        ~LaueClassMatrices();
-        Matrix<short>* getMatrix(int pIndex);
+private:
+  Array<MatrixReader*>* iMatrices;
+public:
+  LaueClassMatrices();
+  ~LaueClassMatrices();
+  Matrix<short>* getMatrix(unsigned int pIndex) const;
+  //  Array<Matrix<short>*>* getList(unsigned int pIndex) const;
 };
 
+/*
+ * Creates a large array containing all the unique matrices.
+ */
 LaueClassMatrices::LaueClassMatrices()
 {
-    iMatrices = new Matrix<short>*[39];
-    
-    iMatrices[0] = new MatrixReader("[-1, 0, 0; -1, 1, 0; 0, 0, 1]");
-    iMatrices[1] = new MatrixReader("[-1, 0, 0; 0, -1, 0; 0, 0, 1]");
-    iMatrices[2] = new MatrixReader("[-1, 0, 0; 0, 0, 1; 0, -1, 0]");
-    iMatrices[3] = new MatrixReader("[-1, 0, 0; 0, 1, 0; 0, 0, -1]");
-    iMatrices[4] = new MatrixReader("[-1, 0, 0; 0, 1, 0; 0, 0, 1]");
-    iMatrices[5] = new MatrixReader("[-1, 1, 0; -1, 0, 0; 0, 0, 1]");
-    iMatrices[6] = new MatrixReader("[-1, 1, 0; -1, 1, 0; 0, 0, 1]");
-    iMatrices[7] = new MatrixReader("[-1, 1, 0; 0, 1, 0; 0, 0, 1]");
-    iMatrices[8] = new MatrixReader("[0, -1, 0; -1, 0, 0; 0, 0, 1]");
-    iMatrices[9] = new MatrixReader("[0, -1, 0; 0, 0, 1; -1, 0, 0]");
-    iMatrices[10] = new MatrixReader("[0, -1, 0; 1, -1, 0; 0, 0, 1]");
-    iMatrices[11] = new MatrixReader("[0, -1, 0; 1, 0, 0; 0, 0, -1]");
-    iMatrices[12] = new MatrixReader("[0, -1, 0; 1, 0, 0; 0, 0, 1]");
-    iMatrices[13] = new MatrixReader("[0, 0, -1; 0, 1, 0; -1, 0, 0]");
-    iMatrices[14] = new MatrixReader("[0, 0, -1; 1, 0, 0; 0, -1, 0]");
-    iMatrices[15] = new MatrixReader("[0, 0, 1; -1, 0, 0; 0, -1, 0]");
-    iMatrices[16] = new MatrixReader("[0, 0, 1; 0, -1, 0; -1, 0, 0]");
-    iMatrices[17] = new MatrixReader("[0, 0, 1; 0, 1, 0; -1, 0, 0]");
-    iMatrices[18] = new MatrixReader("[0, 0, 1; 0, 1, 0; 1, 0, 0]");
-    iMatrices[19] = new MatrixReader("[0, 0, 1; 1, 0, 0; 0, -1, 0]");
-    iMatrices[20] = new MatrixReader("[0, 0, 1; 1, 0, 0; 0, 1, 0]");
-    iMatrices[21] = new MatrixReader("[0, 1, 0; -1, 0, 0; 0, 0, -1]");
-    iMatrices[22] = new MatrixReader("[0, 1, 0; -1, 0, 0; 0, 0, 1]");
-    iMatrices[23] = new MatrixReader("[0, 1, 0; -1, 1, 0; 0, 0, 1]");
-    iMatrices[24] = new MatrixReader("[0, 1, 0; 0, 0, -1; -1, 0, 0]");
-    iMatrices[25] = new MatrixReader("[0, 1, 0; 0, 0, 1; -1, 0, 0]");
-    iMatrices[26] = new MatrixReader("[0, 1, 0; 0, 0, 1; 1, 0, 0]");
-    iMatrices[27] = new MatrixReader("[0, 1, 0; 1, 0, 0; 0, 0, -1]");
-    iMatrices[28] = new MatrixReader("[0, 1, 0; 1, 0, 0; 0, 0, 1]");
-    iMatrices[29] = new MatrixReader("[1, -1, 0; 0, -1, 0; 0, 0, 1]");
-    iMatrices[30] = new MatrixReader("[1, -1, 0; 1, 0, 0; 0, 0, 1]");
-    iMatrices[31] = new MatrixReader("[1, 0, 0; 0, -1, 0; 0, 0, -1]");
-    iMatrices[32] = new MatrixReader("[1, 0, 0; 0, -1, 0; 0, 0, 1]");
-    iMatrices[33] = new MatrixReader("[1, 0, 0; 0, 0, -1; 0, -1, 0]");
-    iMatrices[34] = new MatrixReader("[1, 0, 0; 0, 0, 1; 0, -1, 0]");
-    iMatrices[35] = new MatrixReader("[1, 0, 0; 0, 0, 1; 0, 1, 0]");
-    iMatrices[36] = new MatrixReader("[1, 0, 0; 0, 1, 0; 0, 0, -1]");
-    iMatrices[37] = new MatrixReader("[1, 0, 0; 0, 1, 0; 0, 0, 1]");
-    iMatrices[38] = new MatrixReader("[1, 0, 0; 1, -1, 0; 0, 0, 1]");
+    MatrixReader* tMatrices[39];
+ 
+    tMatrices[0] = new MatrixReader("[-1 0 0; -1 1 0; 0 0 1]");
+    tMatrices[1] = new MatrixReader("[-1 0 0; 0 -1 0; 0 0 1]");
+    tMatrices[2] = new MatrixReader("[-1 0 0; 0 0 1; 0 -1 0]");
+    tMatrices[3] = new MatrixReader("[-1 0 0; 0 1 0; 0 0 -1]");
+    tMatrices[4] = new MatrixReader("[-1 0 0; 0 1 0; 0 0 1]");    
+    tMatrices[5] = new MatrixReader("[-1 1 0; -1 0 0; 0 0 1]");
+    tMatrices[6] = new MatrixReader("[-1 1 0; -1 1 0; 0 0 1]");
+    tMatrices[7] = new MatrixReader("[-1 1 0; 0 1 0; 0 0 1]");
+    tMatrices[8] = new MatrixReader("[0 -1 0; -1 0 0; 0 0 1]");
+    tMatrices[9] = new MatrixReader("[0 -1 0; 0 0 1; -1 0 0]");
+    tMatrices[10] = new MatrixReader("[0 -1 0; 1 -1 0; 0 0 1]");
+    tMatrices[11] = new MatrixReader("[0 -1 0; 1 0 0; 0 0 -1]");
+    tMatrices[12] = new MatrixReader("[0 -1 0; 1 0 0; 0 0 1]");
+    tMatrices[13] = new MatrixReader("[0 0 -1; 0 1 0; -1 0 0]");
+    tMatrices[14] = new MatrixReader("[0 0 -1; 1 0 0; 0 -1 0]");
+    tMatrices[15] = new MatrixReader("[0 0 1; -1 0 0; 0 -1 0]");
+    tMatrices[16] = new MatrixReader("[0 0 1; 0 -1 0; -1 0 0]");
+    tMatrices[17] = new MatrixReader("[0 0 1; 0 1 0; -1 0 0]");
+    tMatrices[18] = new MatrixReader("[0 0 1; 0 1 0; 1 0 0]");
+    tMatrices[19] = new MatrixReader("[0 0 1; 1 0 0; 0 -1 0]");
+    tMatrices[20] = new MatrixReader("[0 0 1; 1 0 0; 0 1 0]");
+    tMatrices[21] = new MatrixReader("[0 1 0; -1 0 0; 0 0 -1]");
+    tMatrices[22] = new MatrixReader("[0 1 0; -1 0 0; 0 0 1]");
+    tMatrices[23] = new MatrixReader("[0 1 0; -1 1 0; 0 0 1]");
+    tMatrices[24] = new MatrixReader("[0 1 0; 0 0 -1; -1 0 0]");
+    tMatrices[25] = new MatrixReader("[0 1 0; 0 0 1; -1 0 0]");
+    tMatrices[26] = new MatrixReader("[0 1 0; 0 0 1; 1 0 0]");
+    tMatrices[27] = new MatrixReader("[0 1 0; 1 0 0; 0 0 -1]");
+    tMatrices[28] = new MatrixReader("[0 1 0; 1 0 0; 0 0 1]");
+    tMatrices[29] = new MatrixReader("[1 -1 0; 0 -1 0; 0 0 1]");
+    tMatrices[30] = new MatrixReader("[1 -1 0; 1 0 0; 0 0 1]");
+    tMatrices[31] = new MatrixReader("[1 0 0; 0 -1 0; 0 0 -1]");
+    tMatrices[32] = new MatrixReader("[1 0 0; 0 -1 0; 0 0 1]");
+    tMatrices[33] = new MatrixReader("[1 0 0; 0 0 -1; 0 -1 0]");
+    tMatrices[34] = new MatrixReader("[1 0 0; 0 0 1; 0 -1 0]");
+    tMatrices[35] = new MatrixReader("[1 0 0; 0 0 1; 0 1 0]");
+    tMatrices[36] = new MatrixReader("[1 0 0; 0 1 0; 0 0 -1]");
+    tMatrices[37] = new MatrixReader("[1 0 0; 0 1 0; 0 0 1]");
+    tMatrices[38] = new MatrixReader("[1 0 0; 1 -1 0; 0 0 1]");
+    iMatrices = new Array<MatrixReader*>(tMatrices, 39);
+}
+
+Matrix<short>* LaueClassMatrices::getMatrix(unsigned int pIndex) const
+{
+    return (Matrix<short>*)((*iMatrices)[pIndex]);
 }
 
 LaueClassMatrices::~LaueClassMatrices()
 {
     for (int i = 0; i < 39; i ++)
     {
-        delete iMatrices[i];
+        delete (*iMatrices)[i];
     }
+    delete iMatrices;
 }
 
+/*
+ * the laue group of matrices
+ */
+class LaueGroups::LaueGroup
+{
+public:
+  unsigned short iCrystalSystem;
+  Array<unsigned short>* iMatIndices;
+  
+  LaueGroup(const unsigned short pSys, const unsigned short pIndices[], const int pNumMat):iCrystalSystem(pSys)
+  {
+      iMatIndices = new Array<unsigned short>(pIndices, pNumMat);
+  }
+  
+  unsigned short getCrystalSystem() const
+  {
+      return iCrystalSystem;
+  }
+  
+  Array<unsigned short>& getMatIndices() const
+  {
+      return *iMatIndices;
+  }
+  
+  std::ostream& output(std::ostream& pStream)
+  {
+      return pStream << "LaueGroup System " << iCrystalSystem << "\n";
+  }
+
+  ~LaueGroup()
+  {
+      delete iMatIndices;
+  }
+};
+
+LaueGroups::LaueGroups()
+{
+    LaueGroup* tArray[12];
+    const unsigned short tIndices[] = {37, //Triclinic
+				       3, //1 2/m 1
+				       4, 32, 1, //2/m 2/m 2/m
+				       11, 1, 22, //4/m
+				       12, 1, 4, 22, 28, 32, 8,//4/m m m
+				       10, 5, //-3
+				       10, 5, 7, 38, 8, //-3 m 1
+				       10, 5, 28, 29, 0, //  -3 1 m
+				       30, 10, 1, 6, 23, //6/m
+				       30, 10, 1, 5, 23, 7, 28, 38, 29, 8, 0, //6/m m m
+				       26, 20, 36, 25, 19, 24, 15, 14, 3, 9, 31,//m -3
+				       26, 20, 36, 25, 19, 24, 15, 14, 3, 9, 31, 28, 18, 35, 27, 17, 34, 13, 2, 33, 21, 16, 11};//m -3 m
+    
+    tArray[0] = new LaueGroup(kTriclinicID, tIndices, 1); //Triclinic
+    tArray[1] = new LaueGroup(kMonoclinicBID, &(tIndices[1]), 1); //1 2/m 1
+    tArray[2] = new LaueGroup(kOrtharombicID, &(tIndices[2]), 3); //2/m 2/m 2/m
+    tArray[3] = new LaueGroup(kTetragonalID, &(tIndices[5]), 3); //4/m
+    tArray[4] = new LaueGroup(kTetragonalID, &(tIndices[8]), 7); //4/m m m
+    tArray[5] = new LaueGroup(kTrigonalID, &(tIndices[15]), 2); //-3
+    tArray[6] = new LaueGroup(kTrigonalID, &(tIndices[17]), 5); //-3 m 1
+    tArray[7] = new LaueGroup(kTrigonalID, &(tIndices[22]), 5); //  -3 1 m
+    tArray[8] = new LaueGroup(kHexagonalID, &(tIndices[27]), 5); //6/m
+    tArray[9] = new LaueGroup(kHexagonalID, &(tIndices[32]), 11); //6/m m m
+    tArray[10] = new LaueGroup(kCubicID, &(tIndices[43]), 11); //m -3
+    tArray[11] = new LaueGroup(kCubicID, &(tIndices[54]), 23); //m -3 m
+    iGroups = new Array<LaueGroup*>(tArray, 12);
+}
+
+Array<unsigned short>& LaueGroups::getGroupIndice(const unsigned int pSystemRef, const unsigned short pGroupNum) const
+{
+    return (*iGroups)[pSystemRef+pGroupNum]->getMatIndices();
+}
+
+LaueGroups::~LaueGroups()
+{
+    for (unsigned int i = 0; i < iGroups->size(); i++)
+    {
+	delete (*iGroups)[i];
+    }
+    delete iGroups;
+}
+
+unsigned int LaueGroups::getSystemRef(unsigned short* pNumGroup, const unsigned short pSystem) const
+{
+    unsigned short i = 0;
+    unsigned short tResult;
+
+    (*pNumGroup) = 0;
+    while (i < iGroups->size() && (*iGroups)[i]->getCrystalSystem() != pSystem)
+    {i++;}
+    tResult = i;
+    while (i < iGroups->size() && (*iGroups)[i]->getCrystalSystem()==pSystem)
+    {(*pNumGroup)++; i++;};
+    if ((*pNumGroup) > 0)
+    {
+	return tResult;
+    }
+    return 0;
+}
+
+MergedData LaueGroups::mergeSystemGroup(const HKLData& pHKLData, const unsigned short pSystemRef, const unsigned short pGroupID) const
+{
+    MergedData tResult(pHKLData, (*iGroups)[pSystemRef+pGroupID]->getMatIndices());
+    return tResult;
+}
+
+/*
+ * Calculates the sum of the abs differences in an array of floats.
+ */
 float sumdiff(Array<float>& pValues, float pMean)
 {
     float tTotal = 0;
@@ -190,16 +210,16 @@ float sumdiff(Array<float>& pValues, float pMean)
     return tTotal;
 }
 
-
-MergedData::MergedData(const HKLData& pData, vector<Matrix<short>*>& pMatrice)
+MergedData::MergedData(const HKLData& pData, Array<unsigned short>& pMatIndices)
 {
     long tReflNum = pData.numberOfReflections();
     multiset<Reflection*, lsreflection>* tSortedReflections = new multiset<Reflection*, lsreflection>();
     Reflection* tReflections = new Reflection[tReflNum];
+
     for (int i = 0; i < tReflNum; i++)
     {
         //Reflection tNewReflection =
-        equivalentise(*pData.getReflection(i), pMatrice, &(tReflections[i]));
+        equivalentise(*pData.getReflection(i), pMatIndices, &(tReflections[i]));
         tSortedReflections->insert(&(tReflections[i]));
     }
     
@@ -213,27 +233,37 @@ MergedData::MergedData(const HKLData& pData, vector<Matrix<short>*>& pMatrice)
         float tSumSum = 0;
         float tMeanDiffSum = 0;
         
-        while (tIter != tSortedReflections->end())
+        while (tIter != tSortedReflections->end()) //Run through all the reflections
         {
-            if (!(*((*tIter)->tHKL) == tCurHKL))
-            {
-                float tSum = sum(tValues.getPointer(), tValues.size());
-                tSumSum += tSum;
-                tMeanDiffSum += sumdiff(tValues, tSum/tValues.size());
-                tCurHKL = *((*tIter)->tHKL);
-                tValues.clear();
-            }
-           // cout << *(*tIter) << "\n";
-            tValues.add((*tIter)->i);
-            tIter++;
+            if (!(*((*tIter)->tHKL) == tCurHKL))//If the HKL value has changed then 
+	    {
+		if (tValues.size()>0) //As long as there are more then one reflection
+		{
+		    float tSum = sum(tValues.getPointer(), tValues.size());
+		    tSumSum += tSum;
+		    tMeanDiffSum += sumdiff(tValues, tSum/tValues.size());
+		}
+		tCurHKL = *((*tIter)->tHKL); //Save the next hkl value
+		tValues.clear();   //Remove all the old intensities
+	    }
+            tValues.add((*tIter)->i);  //Save the intensity
+            tIter++; //Move on to the next reflection in the list.
         }
-        iRFactor = 100*(tSumSum/tMeanDiffSum);
+        iRFactor = tMeanDiffSum/tSumSum; //In the desciption this is multiplied by
+	//But as these values are for comparison there isn't much point
     }
- //   cout << "\nR factor: " << iRFactor << "\n";
     delete[] tReflections;
 }
 
+MergedData::MergedData(const MergedData& pMergedData) //Copy constructure
+{
+    iRFactor = pMergedData.iRFactor;
+}
 
+/*
+ * returns whether the HKL pMat1 is greater then HKL pMat2
+ * (H1 > H2) || ((H1 == H2) && ((K1 >K2) || ((K1 == K2) && (L1 > L2)))  
+ */
 inline bool greaterHKL(Matrix<short> pMat1, Matrix<short> pMat2)
 {
     if (pMat1.getValue(0) < pMat2.getValue(0))
@@ -257,33 +287,36 @@ inline bool greaterHKL(Matrix<short> pMat1, Matrix<short> pMat2)
     return false;
 }
 
-void MergedData::equivalentise(const Reflection& pReflection, vector<Matrix<short>*>& pMatrice, Reflection* pResult)
+LaueClassMatrices gLaueMatrices;
+/*
+ *  Applies a vector of matrices to the hkl values of a reflection.
+ * The greatest of these results are returned in pResult with the same 
+ * I and sigma(I) values passed.
+ */
+void MergedData::equivalentise(const Reflection& pReflection, Array<unsigned short>& pMatIndices, Reflection* pResult) const
 {
     Matrix<short> tMaxHKL;
-    tMaxHKL = *(pReflection.getHKL());
+    tMaxHKL = *(pReflection.getHKL()); //Get a copy of the HKL values
     Matrix<short> tCurrentHKL;
-    tCurrentHKL = *(pReflection.getHKL());
-    Matrix<short> tTempHKL(1, 3);
-    static Matrix<short> tZero(1, 3, 0);
+    tCurrentHKL = *(pReflection.getHKL()); //Make another copy of the HKL values
+    Matrix<short> tTempHKL(1, 3); //for storing the results in.
+    static Matrix<short> tZero(1, 3, 0);  //inverting the hkl values
 
-    for (size_t i = 0; i < pMatrice.size(); i++)
+    for (size_t i = 0; i < pMatIndices.size(); i++) //Run through all the matrices.
     {
-        (pMatrice[i])->mul(tCurrentHKL, tTempHKL);
-        for (int j = 0; j < 2; j++)
+        (gLaueMatrices.getMatrix(pMatIndices[i]))->mul(tCurrentHKL, tTempHKL); //Multiply the hkl value with the current matrix
+        for (int j = 0; j < 2; j++) //Do this twice
         {
-            if (!greaterHKL(tTempHKL, tMaxHKL))
+            if (!greaterHKL(tTempHKL, tMaxHKL)) //see if this new HKL value is greater then the last. This is just for consistancy could just as well be least hkl value or something.
             {
                 tMaxHKL = tTempHKL;
             }
-            tZero.sub(tTempHKL, tTempHKL);
+            tZero.sub(tTempHKL, tTempHKL); //Invert the hkl value.
         }
     }
-    pResult->i = pReflection.i;
-    pResult->iSE = pReflection.iSE;
-    //Reflection tReflection(pReflection);
-    //(*tReflection.tHKL) = tMaxHKL;
-    (*pResult->tHKL) = tMaxHKL;
-   // return tReflection;
+    pResult->i = pReflection.i; //Copy I for result
+    pResult->iSE = pReflection.iSE; //Copy sigma(I) for result
+    (*pResult->tHKL) = tMaxHKL; //Set new HKL value
 }
 
 std::ostream& MergedData::output(std::ostream& pStream)
@@ -295,3 +328,4 @@ std::ostream& operator<<(std::ostream& pStream, MergedData& tData)
 {
     return tData.output(pStream);
 }
+
