4a915e47ddea0765a4ceb1711127e8bbcd4b2d13
richardicooper <richardiancooper@gmail.com>
2003-11-10 17:30:57 +0000

    1. Removed the timeval structure because I'm using windows gay version of it. 2. Changes some int and stuff to size_t where they should have been in the first place.

--- bits/SpaceGroup/PCPort.h
+++ bits/SpaceGroup/PCPort.h
@@ -1,20 +1,18 @@
-
-
 #if !defined(__PC_PORT_H__)
 #define __PC_PORT_H__
 int prog_main(int argc, const char * argv[]);	//The main function
 
-struct timeval{
-	long tv_sec;	/*seconds since Jan. 1, 1970*/
-	long tv_usec;
-};
+/*struct timeval{
+	long tv_sec;	*//*seconds since Jan. 1, 1970*/
+/*	long tv_usec;
+};*/
 
 struct timezone{
 	int tz_minuteswest; /* of greenwich */
 	int tz_dsttime;		/* type of dst correction to apply*/
 };
 
-extern void bzero(void* pPoint, long pBytes);
+extern void bzero(void* pPoint, size_t pBytes);
 extern char* index(char* pString, int pChar);
 extern int bcmp(const void *b1, const void *b2, size_t pLength);
 extern float rint(float pNumber);
