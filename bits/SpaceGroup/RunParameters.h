e4208fc5cc855f2dff002be92ff04ce48add78c3
richardicooper <richardiancooper@gmail.com>
2003-11-11 11:58:44 +0000

    1. Removed the interactive mode flag from the runParameters class

--- bits/SpaceGroup/RunParameters.h
+++ bits/SpaceGroup/RunParameters.h
@@ -29,7 +29,7 @@ class RunParameters
         Path iOutputFile;		//The path of the file where the stats and ranking should be output to.
         Path iParamFile;		//A path to the file which contains any further parameters.
         bool iChiral;		//False is not nessaseraly chiral. false chiral -nc, -c
-        bool iInteractiveMode;	//If this is set true the program will go into interactive mode at the end. -i
+     //   bool iInteractiveMode;	//If this is set true the program will go into interactive mode at the end. -i
         bool iVerbose;		//If this is set true the program will output the stats table after it has calculated all it's probabilits. -v
 	bool iMerge;            //If this is set true then the HKL data will be merged to try and find the laue group. Default is true. -m
         String iCrystalSys;	//Crystal System
