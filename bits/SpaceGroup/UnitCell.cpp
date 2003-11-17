60e4e969f9bea2f4e6548f59e93d9137067e4117
richardicooper <richardiancooper@gmail.com>
2003-11-17 14:44:12 +0000

    1. Initialized some variables which had been forgotten about.

--- bits/SpaceGroup/UnitCell.cpp
+++ bits/SpaceGroup/UnitCell.cpp
@@ -338,6 +338,12 @@ UnitCell::UnitCell()
     iA = 0;
     iB = 0;
     iC = 0;
+    iSEA = 0;
+    iSEB = 0;
+    iSEC = 0;
+    iSEAlpha = 0;
+    iSEBeta = 0;
+    iSEGamma = 0;
 }
 
 bool UnitCell::init(char* pLine)	//This parases the line which it is passed. Initalising the feilds of the unit cell with it. If the line is not formated correctly then an false is returned.
