fd21970f8a94b32fb2b885e4aa8b2e8560b94f6b
richardicooper <richardiancooper@gmail.com>
2004-01-23 14:02:46 +0000

    1. Made the current Laue Group a global variable. 2. LaueGroups::LaueGroup now extends MyObject like all object should. 3. Made variables in  LaueGroups::LaueGroup private. 4. Added the identity matrix to all the Laue Groups. 5. Correct the matrices for the Laue symmetry to the inverse of what they where previously. This is because cameran outputs the matrices in real space not reciprocal space. 6. Added some comments.

--- bits/SpaceGroup/ReflectionMerging.cpp
+++ bits/SpaceGroup/ReflectionMerging.cpp
@@ -25,7 +25,7 @@
 
 /*
  * returns whether the HKL pMat1 is greater then HKL pMat2
- * (H1 > H2) || ((H1 == H2) && ((K1 >K2) || ((K1 == K2) && (L1 > L2)))  
+ * 
  */
 inline bool greaterHKL(const Matrix<short>& pMat1, const Matrix<short>& pMat2)
 {
@@ -66,7 +66,7 @@ LaueClassMatrices::LaueClassMatrices():iMatrices(NULL)
 {
     MatrixReader* tMatrices[39];
  
-    tMatrices[0] = new MatrixReader("[-1 0 0; -1 1 0; 0 0 1]");
+/*    tMatrices[0] = new MatrixReader("[-1 0 0; -1 1 0; 0 0 1]");
     tMatrices[1] = new MatrixReader("[-1 0 0; 0 -1 0; 0 0 1]");
     tMatrices[2] = new MatrixReader("[-1 0 0; 0 0 1; 0 -1 0]");
     tMatrices[3] = new MatrixReader("[-1 0 0; 0 1 0; 0 0 -1]");
@@ -104,7 +104,48 @@ LaueClassMatrices::LaueClassMatrices():iMatrices(NULL)
     tMatrices[35] = new MatrixReader("[1 0 0; 0 0 1; 0 1 0]");
     tMatrices[36] = new MatrixReader("[1 0 0; 0 1 0; 0 0 -1]");
     tMatrices[37] = new MatrixReader("[1 0 0; 0 1 0; 0 0 1]");
-    tMatrices[38] = new MatrixReader("[1 0 0; 1 -1 0; 0 0 1]");
+    tMatrices[38] = new MatrixReader("[1 0 0; 1 -1 0; 0 0 1]");*/
+tMatrices[0] = new MatrixReader("[-1 -1 0; 0 1 0; 0 0 1]");
+tMatrices[1] = new MatrixReader("[-1 0 0; 0 -1 0; 0 0 1]");
+tMatrices[2] = new MatrixReader("[-1 0 0; 0 0 1; 0 -1 0]");
+tMatrices[3] = new MatrixReader("[-1 0 0; 0 1 0; 0 0 -1]");
+tMatrices[4] = new MatrixReader("[-1 0 0; 0 1 0; 0 0 1]");
+tMatrices[5] = new MatrixReader("[0 1 0; -1 -1 0; 0 0 1]");
+tMatrices[6] = new MatrixReader("[-1 1 0; 1 0 0; 0 0 1]");
+tMatrices[7] = new MatrixReader("[-1 0 0; 1 1 0; 0 0 1]");
+tMatrices[8] = new MatrixReader("[0 -1 0; -1 0 0; 0 0 1]");
+tMatrices[9] = new MatrixReader("[0 -1 0; 0 0 1; -1 0 0]");
+tMatrices[10] = new MatrixReader("[-1 -1 0; 1 0 0; 0 0 1]");
+tMatrices[11] = new MatrixReader("[0 -1 0; 1 0 0; 0 0 -1]");
+tMatrices[12] = new MatrixReader("[0 -1 0; 1 0 0; 0 0 1]");
+tMatrices[13] = new MatrixReader("[0 0 -1; 0 1 0; -1 0 0]");
+tMatrices[14] = new MatrixReader("[0 0 -1; 1 0 0; 0 -1 0]");
+tMatrices[15] = new MatrixReader("[0 0 1; -1 0 0; 0 -1 0]");
+tMatrices[16] = new MatrixReader("[0 0 1; 0 -1 0; -1 0 0]");
+tMatrices[17] = new MatrixReader("[0 0 1; 0 1 0; -1 0 0]");
+tMatrices[18] = new MatrixReader("[0 0 1; 0 1 0; 1 0 0]");
+tMatrices[19] = new MatrixReader("[0 0 1; 1 0 0; 0 -1 0]");
+tMatrices[20] = new MatrixReader("[0 0 1; 1 0 0; 0 1 0]");
+tMatrices[21] = new MatrixReader("[0 1 0; -1 0 0; 0 0 -1]");
+tMatrices[22] = new MatrixReader("[0 1 0; -1 0 0; 0 0 1]");
+tMatrices[23] = new MatrixReader("[1 1 0; -1 0 0; 0 0 1]");
+tMatrices[24] = new MatrixReader("[0 1 0; 0 0 -1; -1 0 0]");
+tMatrices[25] = new MatrixReader("[0 1 0; 0 0 1; -1 0 0]");
+tMatrices[26] = new MatrixReader("[0 1 0; 0 0 1; 1 0 0]");
+tMatrices[27] = new MatrixReader("[0 1 0; 1 0 0; 0 0 -1]");
+tMatrices[28] = new MatrixReader("[0 1 0; 1 0 0; 0 0 1]");
+tMatrices[29] = new MatrixReader("[1 0 0; -1 -1 0; 0 0 1]");
+tMatrices[30] = new MatrixReader("[0 -1 0; 1 1 0; 0 0 1]");
+tMatrices[31] = new MatrixReader("[1 0 0; 0 -1 0; 0 0 -1]");
+tMatrices[32] = new MatrixReader("[1 0 0; 0 -1 0; 0 0 1]");
+tMatrices[33] = new MatrixReader("[1 0 0; 0 0 -1; 0 -1 0]");
+tMatrices[34] = new MatrixReader("[1 0 0; 0 0 1; 0 -1 0]");
+tMatrices[35] = new MatrixReader("[1 0 0; 0 0 1; 0 1 0]");
+tMatrices[36] = new MatrixReader("[1 0 0; 0 1 0; 0 0 -1]");
+tMatrices[37] = new MatrixReader("[1 0 0; 0 1 0; 0 0 1]");
+tMatrices[38] = new MatrixReader("[1 1 0; 0 -1 0; 0 0 1]");
+
+
     iMatrices = new Array<MatrixReader*>(tMatrices, 39);
 }
 
@@ -122,19 +163,20 @@ LaueClassMatrices::~LaueClassMatrices()
     delete iMatrices;
 }
 
+char* gLaueGroup;
+
 static LaueClassMatrices gLaueMatrices;
 /*
  * the laue group of matrices
  */
-class LaueGroups::LaueGroup
+class LaueGroups::LaueGroup:public MyObject
 {
-public:
-    
+private:
     LaueGroups::systemID iCrystalSystem;
     Array<unsigned short>* iMatIndices;
     MergedData* iMergedData;
     char* iLaueGroup;
-    
+public:    
     LaueGroup(const LaueGroups::systemID pSys, const char* pLaueGroup, const unsigned short pIndices[], const int pNumMat):iCrystalSystem(pSys), iMergedData(NULL)
     {
         iMatIndices = new Array<unsigned short>(pIndices, pNumMat);
@@ -181,6 +223,7 @@ public:
     
     float getRFactor() const
     {
+        gLaueGroup = iLaueGroup;
         if (iMergedData)
             return iMergedData->getRFactor();
         return -1;
@@ -235,12 +278,23 @@ public:
                     continue;
                 }
             }
-            tCurHKL = (*pHKLs.get(j)->tHKL);
+            tCurHKL = (*pHKLs.get(j)->tHKL);   //Set current max hkl value
+           // Matrix<short> tOutputMatrix(3, 1);
             for (size_t i = 0; i < iMatIndices->size(); i++) //Run through all the matrices.
             {
+            //    tCurHKL = (*pHKLs.get(j)->tHKL);
                 (gLaueMatrices.getMatrix((*iMatIndices)[i]))->mul(*pHKLs.get(j)->tHKL, tTempHKL); //Multiply the hkl value with the current matrix
+                
+              //  Matrix<short> tMatrix(*(gLaueMatrices.getMatrix((*iMatIndices)[i])));
+                //tMatrix.transpose();
+                //tMatrix.mul(*pHKLs.get(j)->tHKL, tTempHKL); //Multiply the hkl value with the current matrix
+               // cout << (*iMatIndices)[i] <<  "\n" << (*gLaueMatrices.getMatrix((*iMatIndices)[i])) << "\n";
                 for (int j = 0; j < 2; j++) //Do this twice
                 {
+   
+                  //  tOutputMatrix = tTempHKL;
+                 //   tOutputMatrix.transpose();
+                  //  cout << "\t" << tOutputMatrix << "\n";
                     if (greaterHKL(tTempHKL, tCurHKL)) //see if this new HKL value is greater then the last. This is just for consistancy could just as well be least hkl value or something.
                     {
                         tCurHKL = tTempHKL;
@@ -262,13 +316,13 @@ public:
         float tScalarDiff = 0;
         for (size_t i = 0; i < iMatIndices->size(); i++) //Run through all the matrices.
         {   
-            tOperator = (*gLaueMatrices.getMatrix((*iMatIndices)[i])); //Multiply the hkl value with the current matrix
+            tOperator = (*gLaueMatrices.getMatrix((*iMatIndices)[i])); //Get the current metrix operator.
             tOperatorTranspose = tOperator;
-            tOperatorTranspose.transpose();
-            tOperatorTranspose.mul(tMetricTensor, tDiff);
-            tDiff.mul(tOperator, tOperatorTranspose);
+            tOperatorTranspose.transpose(); //Transpose the matrix
+            tOperatorTranspose.mul(tMetricTensor, tDiff); //Multiply the metric tensor by the current transpose of the matrix
+            tDiff.mul(tOperator, tOperatorTranspose); //Multiply the metric tensor by the current matrix
             
-            tMetricTensor.sub(tOperatorTranspose, tDiff);
+            tMetricTensor.sub(tOperatorTranspose, tDiff); 
             tScalarDiff = max(tDiff.abssum(), tScalarDiff);
         }
         return tScalarDiff;
@@ -279,35 +333,35 @@ LaueGroups::LaueGroups()//:Array<LaueGroup*>(14)
 {
     LaueGroup* tArray[14];
     const unsigned short tIndices[] = {37, //Triclinic
-				       31, //2/m 1 1
-                                       3, //1 2/m 1
-                                       1, //1 1 2/m
-				       4, 32, 1, //2/m 2/m 2/m
-				       11, 1, 22, //4/m
-				       12, 1, 4, 22, 28, 32, 8,//4/m m m
-				       10, 5, //-3
-				       10, 5, 7, 38, 8, //-3 m 1
-				       10, 5, 28, 29, 0, //  -3 1 m
-				       30, 10, 1, 6, 23, //6/m
-				       30, 10, 1, 5, 23, 7, 28, 38, 29, 8, 0, //6/m m m
-				       26, 20, 36, 25, 19, 24, 15, 14, 3, 9, 31,//m -3
-				       26, 20, 36, 25, 19, 24, 15, 14, 3, 9, 31, 28, 18, 35, 27, 17, 34, 13, 2, 33, 21, 16, 11};//m -3 m
+				       37, 31, //2/m 1 1
+                                       37, 3, //1 2/m 1
+                                       37, 1, //1 1 2/m
+				       37, 4, 32, 1, //2/m 2/m 2/m
+				       37, 11, 1, 22, //4/m
+				       37, 12, 1, 4, 22, 28, 32, 8,//4/m m m
+				       37, 10, 5, //-3
+				       37, 10, 5, 7, 38, 8, //-3 m 1
+				       37, 10, 5, 28, 29, 0, //  -3 1 m
+				       37, 30, 10, 1, 6, 23, //6/m
+				       37, 30, 10, 1, 5, 23, 7, 28, 38, 29, 8, 0, //6/m m m
+				       37, 26, 20, 36, 25, 19, 24, 15, 14, 3, 9, 31,//m -3
+				       37, 26, 20, 36, 25, 19, 24, 15, 14, 3, 9, 31, 28, 18, 35, 27, 17, 34, 13, 2, 33, 21, 16, 11};//m -3 m
     /* The laue groups should always bin in order of symmetry as other methods rely this order on this*/
     tArray[0] = new LaueGroup(kTriclinicID, "-1", tIndices, 1); //Triclinic
-    tArray[1] = new LaueGroup(kMonoclinicAID, "2/m 1 1", &(tIndices[1]), 1); //2/m 1 1
-    tArray[2] = new LaueGroup(kMonoclinicBID, "1 2/m 1", &(tIndices[2]), 1); //1 2/m 1
-    tArray[3] = new LaueGroup(kMonoclinicCID, "1 1 2/m", &(tIndices[3]), 1); //1 1 2/m
-    tArray[4] = new LaueGroup(kOrtharombicID, "2/m 2/m 2/m", &(tIndices[4]), 3); //2/m 2/m 2/m
-    tArray[5] = new LaueGroup(kTetragonalID, "4/m", &(tIndices[7]), 3); //4/m
-    tArray[6] = new LaueGroup(kTetragonalID, "4/m m m", &(tIndices[10]), 7); //4/m m m
-    tArray[7] = new LaueGroup(kTrigonalID, "-3", &(tIndices[17]), 2); //-3
-    tArray[8] = new LaueGroup(kTrigonalID, "-3 m 1", &(tIndices[19]), 5); //-3 m 1
-    tArray[9] = new LaueGroup(kTrigonalID, "-3 1 m", &(tIndices[24]), 5); //  -3 1 m
-    tArray[10] = new LaueGroup(kHexagonalID, "6/m", &(tIndices[29]), 5); //6/m
-    tArray[11] = new LaueGroup(kHexagonalID, "6/m m m", &(tIndices[34]), 11); //6/m m m
-    tArray[12] = new LaueGroup(kCubicID, "m -3", &(tIndices[45]), 11); //m -3
-    tArray[13] = new LaueGroup(kCubicID, "m -3 m", &(tIndices[56]), 23); //m -3 m
-//    set(tArray, 14);
+    tArray[1] = new LaueGroup(kMonoclinicAID, "2/m 1 1", &(tIndices[1]), 2); //2/m 1 1
+    tArray[2] = new LaueGroup(kMonoclinicBID, "1 2/m 1", &(tIndices[3]), 2); //1 2/m 1
+    tArray[3] = new LaueGroup(kMonoclinicCID, "1 1 2/m", &(tIndices[5]), 2); //1 1 2/m
+    tArray[4] = new LaueGroup(kOrtharombicID, "2/m 2/m 2/m", &(tIndices[7]), 4); //2/m 2/m 2/m
+    tArray[5] = new LaueGroup(kTetragonalID, "4/m", &(tIndices[11]), 3); //4/m
+    tArray[6] = new LaueGroup(kTetragonalID, "4/m m m", &(tIndices[15]), 8); //4/m m m
+    tArray[7] = new LaueGroup(kTrigonalID, "-3", &(tIndices[23]), 3); //-3
+    tArray[8] = new LaueGroup(kTrigonalID, "-3 m 1", &(tIndices[26]), 6); //-3 m 1
+    tArray[9] = new LaueGroup(kTrigonalID, "-3 1 m", &(tIndices[32]), 6); //  -3 1 m
+    tArray[10] = new LaueGroup(kHexagonalID, "6/m", &(tIndices[38]), 6); //6/m
+    tArray[11] = new LaueGroup(kHexagonalID, "6/m m m", &(tIndices[44]), 12); //6/m m m
+    tArray[12] = new LaueGroup(kCubicID, "m -3", &(tIndices[56]), 12); //m -3
+    tArray[13] = new LaueGroup(kCubicID, "m -3 m", &(tIndices[68]), 24); //m -3 m
+
     iGroups = new Array<LaueGroup*>(tArray, 14);
 }
 
@@ -456,7 +510,7 @@ LaueGroups::systemID LaueGroups::guessSystem(const HKLData& pHKLs, const RunPara
         tRFactor = (*iGroups)[i]->getRFactor();
         if (tRFactor >= 0)
         {
-            if (tRFactor == 0 || tRFactor/tLowestRF < 10)
+            if (tRFactor == 0 || tRFactor/tLowestRF < 10) //If the R-Factor is not equal to 0
             {
                 tBest = i;
             }
@@ -579,37 +633,40 @@ float MergedData::calculateRFactor()
 {
 	static Matrix<short>* tCurHKL;
         static Array<float> tValues(23);                //I don't think that this should need to be any greater then 23 elements long.
-        tCurHKL = (*(gSortedReflections->begin()))->tHKL; //Save the pointer to the current hkl value
         tValues.clear();                                //Clear the values from the last time they were used.
         multiset<Reflection*, lsreflection>::iterator tIter = gSortedReflections->begin(); //Start at the first 
-        float tSumSum = 0;                              //Make sure that every thing is set to 0
-        float tMeanDiffSum = 0;
-        float tSum = 0;
+        register float tSumSum = 0;                              //Make sure that every thing is set to 0
+        register float tMeanDiffSum = 0;
+        register float tSum = 0;
         
+        int tNumMerged = 0, tNumResRef=0;
+        
+        cout << gLaueGroup << "\n";
+        tCurHKL = (*(gSortedReflections->begin()))->tHKL; //Save the pointer to the current hkl value
         while (tIter != gSortedReflections->end()) //Run through all the reflections
         {
-            if (!(*((*tIter)->tHKL) == (*tCurHKL)))//If the HKL value has changed then 
+            if (  !((*(*tIter)->tHKL) == (*tCurHKL))) //If the HKL value has changed then 
 	    {
-                
 		if (tValues.size()>1) //As long as there are more then one reflection
 		{
+                   // cout << tCurHKL->getValue(0) << " " << tCurHKL->getValue(1) << " " << tCurHKL->getValue(2) << "\n";
+                    tNumMerged += tValues.size();
+                    
 		    tSum = fabsf(sum(tValues.getPointer(), tValues.size()));
 		    tSumSum += tSum;
                         
 		    tMeanDiffSum += sumdiff(tValues, tSum/tValues.size());
 		}
-                /*if (abs((*tCurHKL).getValue(0)) == 2 && abs((*tCurHKL).getValue(1)) == 11 && abs((*tCurHKL).getValue(2)) == 5)
-                {
-                    cout << (*tCurHKL) <<" " << tValues.size() << "\n";
-                    cout << "Sum: " << tSum << "\n";
-                    cout << "Sum Of Sums" << tSumSum << "\n";
-                }*/
 		tCurHKL = (*tIter)->tHKL; //Save the next hkl value
 		tValues.clear();   //Remove all the old intensities
+                tNumResRef  ++;
 	    }
             tValues.add((*tIter)->i);  //Save the intensity
             tIter++; //Move on to the next reflection in the list.
         }
+      //  cout << "\n\tNumber of reflections merged: " << (tNumMerged) << "\n";
+        //cout << "\tNumber of merged reflections: " << (tNumResRef) << "\n";
+        //cout << "\tSum of Sum: " << tSumSum << "\n";
         if (tSumSum == 0)
             iRFactor = 0;
         else
