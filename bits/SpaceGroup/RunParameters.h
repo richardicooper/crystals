ea2e48cbe83ecb3e58956cee5de75257afff0da7
richardicooper <richardiancooper@gmail.com>
2004-05-13 10:20:06 +0000

    1. Removed the crystal system field from the runParameter class and and has been replaced with the iLaueGroup field which contains the crystal crystalSystem in it. 2. Added methods to get the the new field and to set it. 3. Added the function getUserNumResponse ask the user for a numerical response.

--- bits/SpaceGroup/RunParameters.h
+++ bits/SpaceGroup/RunParameters.h
@@ -16,12 +16,16 @@
 #endif
 #include "StringClasses.h"
 #include "UnitCell.h"
+#include "LaueClasses.h"
+#include "BaseTypes.h"
 
 class RunParameters
 {
     private:
         bool iRequestChirality;	//If this is true then the user will be asked if the file crystal is chiral.
-        void readParamFile();
+		//SystemID iCrystalSystem;	//Crystal System
+		JJLaueGroup* iLaueGroup; //The LaueGroup identified
+		void readParamFile();
         bool handleArg(int *pPos, int pMax, const char * argv[]);
     public:
         Path iFileName;		//File name for the hkl data.
@@ -29,15 +33,21 @@ class RunParameters
         Path iOutputFile;		//The path of the file where the stats and ranking should be output to.
         Path iParamFile;		//A path to the file which contains any further parameters.
 //        long iFlags;        //New this contains iChiral iVerbose and iMerge
-        bool iChiral;		//False is not nessaseraly chiral. false chiral -nc, -c
+        bool iChiral;		//False is not nessaseraly chiral. -nc, -c
         bool iVerbose;		//If this is set true the program will output the stats table after it has calculated all it's probabilits. -v
-	bool iMerge;            //If this is set true then the HKL data will be merged to try and find the laue group. Default is true. -m
-        String iCrystalSys;	//Crystal System
+		bool iMerge;		//If this is set true then the HKL data will be merged to try and find the laue group. Default is true. -m
         UnitCell iUnitCell;	//The unit cell
+		
+		//void setCrystalSystem(SystemID pSystem);
+		void setLaueGroup(JJLaueGroup* pLaueGroup);
+		SystemID crystalSystem();
+		JJLaueGroup* laueGroup();
         RunParameters();
         void handleArgs(int pArgc, const char* argv[]);
         void getParamsFromUser();
 };
+
+unsigned int getUserNumResponse(const char* pQuestion, const uint pDefault, const Range pAnswerRange);
 #endif
 
 
