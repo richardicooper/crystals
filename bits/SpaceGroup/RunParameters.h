b10028c76a58ad4a71f12ed1b70b6510ed5e4e17
richardicooper <richardiancooper@gmail.com>
2004-06-03 13:02:18 +0000

    1. Renamed all references to JJ classes so that no longer have the JJ on them 2. Added the unitcell to the parameter class. 3. Added an accessor method to the unit cell.

--- bits/SpaceGroup/RunParameters.h
+++ bits/SpaceGroup/RunParameters.h
@@ -24,7 +24,8 @@ class RunParameters
     private:
         bool iRequestChirality;	//If this is true then the user will be asked if the file crystal is chiral.
 		//SystemID iCrystalSystem;	//Crystal System
-		JJLaueGroup* iLaueGroup; //The LaueGroup identified
+		LaueGroup* iLaueGroup; //The LaueGroup identified
+		UnitCell iUnitCell;	//The unit cell
 		void readParamFile();
         bool handleArg(int *pPos, int pMax, const char * argv[]);
     public:
@@ -32,16 +33,16 @@ class RunParameters
         Path iTablesFile;               //The file name of the tables file.
         Path iOutputFile;		//The path of the file where the stats and ranking should be output to.
         Path iParamFile;		//A path to the file which contains any further parameters.
-//        long iFlags;        //New this contains iChiral iVerbose and iMerge
         bool iChiral;		//False is not nessaseraly chiral. -nc, -c
         bool iVerbose;		//If this is set true the program will output the stats table after it has calculated all it's probabilits. -v
 		bool iMerge;		//If this is set true then the HKL data will be merged to try and find the laue group. Default is true. -m
-        UnitCell iUnitCell;	//The unit cell
+        
 		
 		//void setCrystalSystem(SystemID pSystem);
-		void setLaueGroup(JJLaueGroup* pLaueGroup);
+		void setLaueGroup(LaueGroup* pLaueGroup);
 		SystemID crystalSystem();
-		JJLaueGroup* laueGroup();
+		LaueGroup* laueGroup();
+		UnitCell& unitCell();
         RunParameters();
         void handleArgs(int pArgc, const char* argv[]);
         void getParamsFromUser();
