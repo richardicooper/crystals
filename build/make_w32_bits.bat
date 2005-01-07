9a2de4d5ce88b0349fabb7733738a00ef4f78fee
richardicooper <richardiancooper@gmail.com>
2005-01-07 15:47:47 +0000

    Fix build of crysload.

--- build/make_w32_bits.bat
+++ build/make_w32_bits.bat
@@ -2,11 +2,6 @@
 @if "%1" == "clean" goto clean
 @if "%1" == "tidy" goto tidy
 
-@set OPATH=%PATH%
-@set OLIB=%LIB%
-@set PATH=C:\Progra~1\Micros~3\vc98\bin;C:\Progra~1\Micros~3\df98\bin;%PATH%
-@set LIB=C:\Progra~1\Micros~3\vc98\lib;C:\Progra~1\Micros~3\df98\lib;%LIB%
-@set LINK=
 DF /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\sir92\sir92.fpp /Fosir92.o /static /libs:qwin /optimize:4 /nolink
 DF /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\sir92\norm92.fpp /Fonorm92.o /nostatic /libs:qwin /optimize:4 /nolink
 DF sir92.o norm92.o /libs:qwin
@@ -21,17 +16,17 @@ DF /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\rc93\rc93.fpp  /automatic /op
 DF /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\reindex\reindex.fpp /optimize:4 /MD
 DF /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\shelxs\shelxs.fpp /optimize:4 /MD 
 DF /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\shelxs\sxtocry.fpp /optimize:4 /MD
-CL ..\bits\loader\crysload.cc %CDEF% /EHs /W3 /TP /O2 /D"NDEBUG" /ML /link /subsystem:windows shell32.lib advapi32.lib user32.lib 
+CL ..\bits\loader\crysload.cc %CDEF% /EHs /W3 /TP /O2 /D"NDEBUG" /MD /link shell32.lib advapi32.lib user32.lib
+del crysload.obj
+
 copy ..\bits\sir92\form.sda form.dat
 copy ..\bits\rc93\rc93.ssr rc93.src
-@set PATH=%OPATH%
-@set LIB=%OLIB%
 goto exit
 
 :clean
 del cif2cry.exe convplat.exe csd2cry.exe delred.exe dipin.exe ctwin.exe
 del kccdin.exe rc93.exe reindex.exe shelxs.exe sxtocry.exe sir92.exe
-del rc93.src form.dat
+del rc93.src form.dat crysload.exe
 
 :tidy
 del sir92.o norm92.o
