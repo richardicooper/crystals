5b1bc5ec60c62c36b231619e935522981b92bf1d
stefan <stefan.pantos@chem.ox.ac.uk>
2005-02-07 14:19:52 +0000

    1. Changed the type of the interface command queue to be a listenable queue. 2. Added the event for the wx version to the file.

--- gui/cccontroller.h
+++ gui/cccontroller.h
@@ -14,9 +14,11 @@
 #ifdef __BOTHWX__
 #include <wx/app.h>
 #include <wx/thread.h>
+#include <wx/event.h>
 #include "ccthread.h"
 #endif
 
+#include    "cclistenablesafedeque.h"
 #include    "ccsafedeque.h"
 #include    "ccstatus.h"
 #include    "ccrect.h"
@@ -33,6 +35,10 @@
 
 #ifdef __BOTHWIN__
 #include <windows.h>
+#endif
+
+#ifdef __BOTHWX__
+DECLARE_EVENT_TYPE(ccEVT_COMMAND_ADDED, wxNewEventType())
 #endif
 
 class CcModelDoc;
@@ -178,8 +184,7 @@ class   CcController
     list<CrButton*> mDisableableButtonsList;
 
     CcSafeDeque<string> mCrystalsCommandDeq;
-    CcSafeDeque<string> mInterfaceCommandDeq;
-
+    CcListenableSafeDeque<string> mInterfaceCommandDeq;
 };
 
 
@@ -306,6 +311,9 @@ enum
 
 
 //   $Log: not supported by cvs2svn $
+//   Revision 1.47  2005/02/02 15:30:39  stefan
+//   1. Replaces the to deques for the thread comunication with CcSafeDeque.
+//
 //   Revision 1.46  2005/01/23 10:20:24  rich
 //   Reinstate CVS log history for C++ files and header files. Recent changes
 //   are lost from the log, but not from the files!
