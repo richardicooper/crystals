c40e16fc4a9fd9b58c878f3725249fda625fe2a0
djw <david.watkin@chem.ox.ac.uk>
2008-01-28 12:26:12 +0000

    copy .pdf versions

--- build/make_w32_manual.bat
+++ build/make_w32_manual.bat
@@ -1,18 +1,22 @@
+echo on
 @call make_w32_include.bat
 @if "%1" == "clean" goto clean
 @if "%1" == "tidy" goto tidy
 
-@SETLOCAL
-@mkdir manual
-@cd manual
-@set CRYSSRC=..\..
-@perl -w ..\..\manual\mangen.pl faq.man primer.man guide.man crystals.man cameron.man menu.man readme.man workshop.man
-@cd ..
-@copy manual\html\* manual
-@copy ..\manual\*.jpg manual
-@copy ..\manual\*.gif manual
-@copy ..\manual\*.css manual
-@ENDLOCAL
+SETLOCAL
+mkdir manual
+mkdir manual\analyse
+cd manual
+set CRYSSRC=..\..
+perl -w ..\..\manual\mangen.pl faq.man primer.man guide.man crystals.man cameron.man menu.man readme.man workshop.man
+cd ..
+copy    manual\html\* manual
+copy ..\manual\analyse\ manual\analyse
+copy ..\manual\*.jpg manual
+copy ..\manual\*.gif manual
+copy ..\manual\*.css manual
+copy ..\manual\*.pdf manual
+ENDLOCAL
 @goto exit
 
 :clean
