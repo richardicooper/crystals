6a6249f05a50b15dcd635895e8ad96c0c44108b5
djw <david.watkin@chem.ox.ac.uk>
2009-07-09 09:27:15 +0000

    Copy the \analyse\*.images files to build

--- build/make_w32_manual.bat
+++ build/make_w32_manual.bat
@@ -11,11 +11,15 @@ set CRYSSRC=..\..
 perl -w ..\..\manual\mangen.pl faq.man primer.man guide.man crystals.man cameron.man menu.man readme.man workshop.man
 cd ..
 copy    manual\html\* manual
-copy ..\manual\analyse\ manual\analyse
 copy ..\manual\*.jpg manual
 copy ..\manual\*.gif manual
 copy ..\manual\*.css manual
 copy ..\manual\*.pdf manual
+copy ..\manual\analyse\*.html manual\analyse
+copy ..\manual\analyse\*.jpg  manual\analyse
+copy ..\manual\analyse\*.gif  manual\analyse
+copy ..\manual\analyse\*.css  manual\analyse
+copy ..\manual\analyse\*.pdf  manual\analyse
 ENDLOCAL
 @goto exit
 
