27c3315ee18fae6cb2ef300c1e4885fbdc8cce58
richardicooper <richardiancooper@gmail.com>
2011-03-04 05:56:04 +0000

    Event to catch unminimize (restore) event. Ensure all parent windows are unminimized.

--- gui/cxwindow.h
+++ gui/cxwindow.h
@@ -8,6 +8,10 @@
 //   Authors:   Richard Cooper and Ludwig Macko
 //   Created:   22.2.1998 14:43 Uhr
 //   $Log: not supported by cvs2svn $
+//   Revision 1.19  2005/01/23 10:20:24  rich
+//   Reinstate CVS log history for C++ files and header files. Recent changes
+//   are lost from the log, but not from the files!
+//
 //   Revision 1.1.1.1  2004/12/13 11:16:18  rich
 //   New CRYSTALS repository
 //
@@ -166,6 +170,7 @@ class CxWindow : public BASEWINDOW
       void OnUpdateMenuItem(wxUpdateUIEvent &event );
       void OnKeyDown( wxKeyEvent & event );
       void OnKeyUp( wxKeyEvent & event );
+	  void OnIconize(wxIconizeEvent & event);
 
       DECLARE_EVENT_TABLE()
 
