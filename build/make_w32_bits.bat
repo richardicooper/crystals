b1bb3de378f6389640d2290ddd11c937f1903f4c
richardicooper <richardiancooper@gmail.com>
2012-04-09 08:03:04 +0000

    Fixed build of some bits.

--- build/make_w32_bits.bat
+++ build/make_w32_bits.bat
@@ -10,7 +10,7 @@ call "c:\program files\microsoft visual studio\df98\bin\dfvars.bat"
 %F77% /fpp %FDEF% /I..\crystals ..\bits\sir92\sir92.fpp /Fosir92.o /static /libs:qwin /optimize:4 /nolink
 %F77% /fpp %FDEF% /I..\crystals ..\bits\sir92\norm92.fpp /Fonorm92.o /nostatic /libs:qwin /optimize:4 /nolink
 %F77% sir92.o norm92.o /libs:qwin
-%F77% /fpp %FDEF% /I..\crystals ..\bits\cif2cry\cif2cry.fpp /optimize:4 /MD
+%F77% /fpp %FDEF% /I..\crystals /I..\bits\cif2cry ..\bits\cif2cry\cif2cry.fpp /optimize:4 /MD
 rem contour has not been updated to work woth the new graphics/compiler
 rem %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\contour\contour.fpp /optimize:4 /MD
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\convplat\convplat.fpp /optimize:4 /MD
@@ -19,11 +19,11 @@ rem %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\contour\contour.fpp /o
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\dipin\dipin.fpp /optimize:4 /MD
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\hklf5\ctwin.fpp /optimize:4 /MD
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\hklf5\hklf52cry.fpp /optimize:4 /MD
-%F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\foxman\pcf2cry.fpp /optimize:4 /MD
-%F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\kccdin\kccdin.fpp /optimize:4 /MD
-%F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\OxDiff\OxDiffin.fpp /optimize:4 /MD
+%F77% /fpp /define:_%COMPCODE%_ /I..\crystals /I..\bits\foxman ..\bits\foxman\pcf2cry.fpp /optimize:4 /MD
+%F77% /fpp /define:_%COMPCODE%_ /I..\crystals /I..\bits\kccdin ..\bits\kccdin\kccdin.fpp /optimize:4 /MD
+%F77% /fpp /define:_%COMPCODE%_ /I..\crystals /I..\bits\OxDiff ..\bits\OxDiff\OxDiffin.fpp /optimize:4 /MD
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals /I..\bits\Diffractometers\ ..\bits\Diffractometers\diffin.fpp /optimize:4 /MD
-%F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\rc93\rc93.fpp  /automatic /optimize:4 /MD
+%F77% /fpp /define:_%COMPCODE%_ /I..\crystals /I ..\bits\rc93 ..\bits\rc93\rc93.fpp  /automatic /optimize:4 /MD
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\reindex\reindex.fpp /optimize:4 /MD
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\shelxs\shelxs.fpp /optimize:4 /MD 
 %F77% /fpp /define:_%COMPCODE%_ /I..\crystals ..\bits\shelxs\sxtocry.fpp /optimize:4 /MD
