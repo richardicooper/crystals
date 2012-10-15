7885def94dadf8be97f7bd6fdb7984a8bd8c8bf7
richardicooper <richardiancooper@gmail.com>
2012-10-15 12:02:03 +0000

    Fix comment setting for building script.

--- build/make_w32_script.bat
+++ build/make_w32_script.bat
@@ -6,7 +6,7 @@
 @if not exist ..\editor\cryseditor.exe cd ..\editor&&call make_w32.bat&&cd ..
 @ENDLOCAL
 mkdir script
-@FOR %%I IN ( ..\script\*.ssc ) DO ..\editor\cryseditor %%I script\%%~nI.scp code=%COMPCODE% incl=+ excl=- comm=%% strip
+@FOR %%I IN ( ..\script\*.ssc ) DO ..\editor\cryseditor %%I script\%%~nI.scp code=%COMPCODE% incl=+ excl=- comm=%%%% 
 @FOR %%I IN ( ..\script\*.sda ) DO ..\editor\cryseditor %%I script\%%~nI.dat code=%COMPCODE% incl=+ excl=- comm=# strip
 @goto exit
 
