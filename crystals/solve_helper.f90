b781a37df4dcb56084219543c9fcc689d59c043f
pascal22p <pascal22p@parois.net>
2013-10-13 22:17:01 +0000

    cleaning, new cholesky lapack (not used), ifdef on print statements

--- crystals/solve_helper.f90
+++ crystals/solve_helper.f90
@@ -1,19 +1,20 @@
+! The module solve_helper contains methods to invert the normal matrix
 module solve_helper
 contains 
 
 !> code for the inversion of the normal matrix using eigen decomposition
-!! Small eigen values are filtered out based on a condtion number threshold
+!! Small eigen values are filtered out based on a condition number threshold
 subroutine eigen_inversion(nmatrix, nmsize, eigcutoff, nrejected, condition, filtered_condition)
 use m_mrgrnk
 implicit none
+!> Leading dimension of the matrix nmatrix
+integer, intent(in) :: nmsize
 !> On input symmetric real matrix stored in packed format (lower triangle)
 !! aij is stored in AP( i+(2n-j)(j-1)/2) for j <= i.
 !! On output the inverse of the matrix is return
-real, dimension(:), intent(inout) :: nmatrix
-!> Value for the condition number cutoff
+real, dimension(nmsize*(nmsize+1)/2), intent(inout) :: nmatrix
+!> Value for the relqtive precision cutoff (condition number * machine precision)
 real, intent(in) :: eigcutoff
-!> Leading dimension of the matrix nmatrix
-integer, intent(in) :: nmsize
 !> condition number of matrix
 real, intent(out) :: condition
 !> condition number of filtered_condition
@@ -36,7 +37,7 @@ integer, dimension(1) :: liwork
 allocate(preconditioner(nmsize))
 do i=1,nmsize
     j = ((i-1)*(2*(nmsize)-i+2))/2
-    if(nmatrix(1+j)/=0.0) then
+    if(abs(nmatrix(1+j))<epsilon(0.0)) then
         preconditioner(i)=nmatrix(1+j)
     else
         preconditioner(i)=1.0
@@ -65,6 +66,10 @@ do i=1, nmsize
     eigvectors(i, i:nmsize)=nmatrix(1+j:1+k)
 end do
 
+open(666, file='matrix', form="unformatted",access="stream")
+write(666) eigvectors
+close(666)
+
 ! eigen value filtering
 allocate(eigvalues(nmsize))
 !call date_and_time(VALUES=measuredtime)
@@ -76,6 +81,7 @@ call ssyevd ('V', 'L', nmsize, eigvectors, nmsize, eigvalues, &
 allocate(work(nint(lwork(1))))
 allocate(iwork(liwork(1)))
 
+! eigen decomposition
 call ssyevd ('V', 'L', nmsize, eigvectors, nmsize, eigvalues, &
     work, nint(lwork(1)), iwork, liwork(1), info)
 
@@ -89,38 +95,28 @@ deallocate(iwork)
 deallocate(work)
 
 ! eigen values cutoff
-!print *, 'PP* cond number cut off', eigcutoff, &
-!    'max eigen value', eigvalues(nmsize)
-!print *, 'PP* smallest eigen values', eigvalues(1:3)
               
 truncated=.false.
 i=1
 nrejected = 0
 allocate(iwork(nmsize))
-do while(eigvalues(nmsize)/max(0.00001,eigvalues(i))>eigcutoff)
-!    print *, 'PP* Value', eigvalues(i), 'removed, ', &
-!        'old conditioned number:', eigvalues(nmsize)/eigvalues(i)
-    call mrgrnk(abs(eigvectors(:,i)), iwork)
-!    print *, 'PP* Highest eigenvectors components'
-!    do j=nmsize,nmsize-10,-1
-!        print *, iwork(j), eigvectors(iwork(j),i)
-!    end do
+! filtering base on relative precision we want to obtain
+do while(eigvalues(nmsize)/max(tiny(1.0),eigvalues(i))*epsilon(1.0)>eigcutoff)
+    !call mrgrnk(abs(eigvectors(:,i)), iwork)
+	!print *, i, eigvalues(nmsize), eigvalues(i)
     truncated=.true.
-    !print *, 'sum of components**2', sum(svdVT(:,i)**2)
     i=i+1
 end do
 nrejected = i - 1
-condition = eigvalues(nmsize)/max(0.00001,eigvalues(1))
+condition = eigvalues(nmsize)/max(tiny(1.0),eigvalues(1))
 filtered_condition = condition 
 deallocate(iwork)
+
 if(truncated) then
-!    write(123,*) 'PP* condition number on the ',  &
-!        'remaining eigen values ', eigvalues(nmsize)/eigvalues(i)
+    filtered_condition = eigvalues(nmsize)/max(tiny(1.0),eigvalues(i))
     eigvalues(1:i-1)=0.0
     eigvalues(i:nmsize)=1.0/sqrt(eigvalues(i:nmsize))
-    filtered_condition = eigvalues(nmsize)/max(0.00001,eigvalues(i))
 else
-!    print *, 'PP* condition number ', eigvalues(nmsize)/eigvalues(1)
    eigvalues=1.0/sqrt(eigvalues)
 end if
 
@@ -164,6 +160,8 @@ deallocate(preconditioner)
 
 end subroutine
 
+!> code for the inversion of the normal matrix using cholesky decomposition
+!! Original algorithm from crystals + preconditioning
 subroutine cholesky_inversion(nmatrix, nmsize, info)
 implicit none
 !> Leading dimension of the matrix nmatrix
@@ -192,7 +190,7 @@ info=0
 allocate(preconditioner(nmsize))
 do i=1,nmsize
     j = ((i-1)*(2*(nmsize)-i+2))/2
-    if(nmatrix(1+j)/=0.0) then
+    if(abs(nmatrix(1+j))<epsilon(0.0)) then
         preconditioner(i)=nmatrix(1+j)
     else
         preconditioner(i)=1.0
@@ -293,6 +291,8 @@ deallocate(preconditioner)
 
 end subroutine
 
+!> code for the inversion of the normal matrix using LDL^t decomposition
+!! of symmetric matrices
 subroutine LDLT_inversion(nmatrix, nmsize, info)
 implicit none
 !> Leading dimension of the matrix nmatrix
@@ -309,7 +309,8 @@ integer i, j, k, lwork
 real, dimension(:,:), allocatable :: unpacked
 
 real, dimension(:), allocatable :: work
-integer, dimension(:), allocatable :: ipiv
+integer, dimension(:), allocatable :: ipiv, iwork
+real rcond
 integer, external :: ILAENV
 
 integer :: starttime
@@ -322,7 +323,7 @@ info=0
 allocate(preconditioner(nmsize))
 do i=1,nmsize
     j = ((i-1)*(2*(nmsize)-i+2))/2
-    if(nmatrix(1+j)/=0.0) then
+    if(abs(nmatrix(1+j))<epsilon(0.0)) then
         preconditioner(i)=nmatrix(1+j)
     else
         preconditioner(i)=1.0
@@ -348,6 +349,10 @@ do i=1, nmsize
     unpacked(i, i+1:nmsize)=nmatrix(1+j+1:1+k)
 end do
 
+!open(666, file='matrix', form="unformatted",access="stream")
+!write(666) unpacked
+!close(666)
+
 !call date_and_time(VALUES=measuredtime)
 !starttime=((measuredtime(5)*3600+measuredtime(6)*60)+measuredtime(7))*1000+measuredtime(8)
 
@@ -356,6 +361,17 @@ lwork = ILAENV( 1, 'SSYTRF', 'L', nmsize, nmsize)
 allocate(work(nmsize*lwork))
 call SSYTRF( 'L', nmsize, unpacked, nmsize, IPIV, WORK, nmsize*lwork, INFO )
 deallocate(work)
+
+allocate(work(2*nmsize))
+allocate(iwork(nmsize))
+call SSYCON( 'L', nmsize, unpacked, nmsize, ipiv, 1.0, rcond, work, iwork, info )
+deallocate(work)
+deallocate(iwork)
+#if defined(_GIL_) || defined(_LIN_)
+print *, 'condition number ', 1.0/rcond
+print *, 'relative error ', 1.0/rcond*epsilon(1.0)
+#endif
+
 allocate(work(nmsize))
 call SSYTRI( 'L', nmsize, unpacked, nmsize, IPIV, WORK, INFO )
 deallocate(ipiv)
@@ -387,5 +403,110 @@ deallocate(preconditioner)
 
 end subroutine
 
+!> code for the inversion of the normal matrix using cholesky decomposition
+!! from lapack
+subroutine choleskyl_inversion(nmatrix, nmsize, info)
+implicit none
+!> Leading dimension of the matrix nmatrix
+integer, intent(in) :: nmsize
+!> On input symmetric real matrix stored in packed format (lower triangle)
+!! aij is stored in AP( i+(2n-j)(j-1)/2) for j <= i.
+!! On output the inverse of the matrix is return
+real, dimension(nmsize*(nmsize+1)/2), intent(inout) :: nmatrix
+!> Status of the calculation. =0 if success
+integer, intent(out) :: info
+
+real, dimension(:), allocatable :: preconditioner
+integer i, j, k
+real, dimension(:,:), allocatable :: unpacked
+
+real, dimension(:), allocatable :: work
+integer, dimension(:), allocatable :: iwork
+real rcond
+
+integer :: starttime
+integer, dimension(8) :: measuredtime
+
+info=0
+
+! preconditioning using diagonal terms
+! Allocate diagonal vector
+allocate(preconditioner(nmsize))
+do i=1,nmsize
+    j = ((i-1)*(2*(nmsize)-i+2))/2
+    if(abs(nmatrix(1+j))<epsilon(0.0)) then
+        preconditioner(i)=nmatrix(1+j)
+    else
+        preconditioner(i)=1.0
+    end if
+end do      
+preconditioner = 1.0/sqrt(preconditioner) 
+
+! Applying C N C
+! N: normal matrix
+! C: diagonal matrix with elements from the N diagonal
+do i=1,nmsize
+    j = ((i-1)*(2*(nmsize)-i+2))/2
+    k = j + nmsize - i
+    nmatrix(1+j:1+k)=preconditioner(i)*preconditioner(i:nmsize)*nmatrix(1+j:1+k)
+end do              
+
+! unpacking lower triangle for memory efficiency
+allocate(unpacked(nmsize, nmsize))
+do i=1, nmsize
+    j = ((i-1)*(2*(nmsize)-i+2))/2
+    k = j + nmsize - i
+    unpacked(i:nmsize, i)=nmatrix(1+j:1+k)
+    !unpacked(i, i+1:nmsize)=nmatrix(1+j+1:1+k)
+end do
+
+!open(666, file='matrix', form="unformatted",access="stream")
+!write(666) unpacked
+!close(666)
+
+!call date_and_time(VALUES=measuredtime)
+!starttime=((measuredtime(5)*3600+measuredtime(6)*60)+measuredtime(7))*1000+measuredtime(8)
+
+! choleski decomposition
+call  SPOTRF( 'L', nmsize, unpacked, nmsize, info )
+
+!condition number
+allocate(work(3*nmsize))
+allocate(iwork(nmsize))
+call SPOCON( 'L', nmsize, unpacked, nmsize, 1.0, rcond, work, iwork, info )
+deallocate(work)
+deallocate(iwork)
+#if defined(_GIL_) || defined(_LIN_)
+print *, 'condition number ', 1.0/rcond
+print *, 'relative error ', 1.0/rcond*epsilon(1.0)
+#endif
+! inversion using previous decomposition
+call  SPOTRI( 'L', nmsize, unpacked, nmsize, info )
+
+!call date_and_time(VALUES=measuredtime)
+!print *, 'PP* ***** LDL ', info  , &
+!&       ((measuredtime(5)*3600+measuredtime(6)*60)+measuredtime(7))*1000+measuredtime(8)-starttime, 'ms'
+
+! Pack normal matrix back into original crystals storage
+do i=1,nmsize
+    j = ((i-1)*(2*nmsize-i+2))/2
+    k = j + nmsize - i
+    nmatrix(1+j:1+k)=unpacked(i:nmsize,i)
+end do    
+
+! revert pre conditioning                   
+! Applying C (C N C)^-1 C to get N^-1
+! N: normal matrix
+! C: diagonal matrix with elements from the N diagonal
+do i=1,nmsize
+    j = ((i-1)*(2*(nmsize)-i+2))/2
+    k = j + nmsize - i
+    nmatrix(1+j:1+k)=preconditioner(i)*preconditioner(i:nmsize)* &
+        nmatrix(1+j:1+k)
+end do  
+            
+deallocate(preconditioner)
+
+end subroutine
 
 end module
