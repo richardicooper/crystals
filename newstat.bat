8b9f33590d75fb8bf5f3d3294431fb4cde14d714
djw <david.watkin@chem.ox.ac.uk>
2015-05-01 09:34:57 +0000

    Modify to run with ecce-64

--- newstat.bat
+++ newstat.bat
@@ -2,9 +2,9 @@ if exist newstat.int del newstat.int
 if exist newstat.lis del newstat.lis
 if exist newstat.lis srt newstat.srt
 
-svn status >newstat.int
+svn status -u >newstat.int
 
-rem ecce newstat.int newstat.lis newstat.dat
-sort newstat.int >newstat.srt
+ ecce-64 newstat.int newstat.lis <newstat.dat
+sort newstat.lis >newstat.srt
 
 if exist newstat.srt type newstat.srt
