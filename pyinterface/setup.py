2bf56ce4d7f580cc31c8c503724f02acc4375be8
richardicooper <richardiancooper@gmail.com>
2016-07-29 08:37:15 +0000

    Move python stuff into separate folder pyinterface.

--- pyinterface/setup.py
+++ pyinterface/setup.py
@@ -8,8 +8,8 @@ from distutils.core import setup, Extension
 
 cryspy_module = Extension('cryspy',
                            sources=['cryspy.c'],
-                           library_dirs=['../p/crystals'], 
-						   libraries=['CrystalsShared','gfortran','openblas'],
+                           library_dirs=['../p/crystals','../p/pyinterface'], 
+						   libraries=['CrystalsPyInterface','CrystalsShared','gfortran','openblas'],
                            )
 	   
 
@@ -19,5 +19,8 @@ setup (name = 'cryspy',
        description = """Level 0 CRYSTALS Python API from SWIG""",
        ext_modules = [cryspy_module],
        py_modules = ["cryspy"],
+#       package_data={'crystals': ['../p/commands.dsc','dsjkds', '../p/crystals.srt']},
+#       include_package_data=True,
+       data_files=[('crystals', ['../p/commands.dsc', '../p/crystals.srt'])],
        )
      
