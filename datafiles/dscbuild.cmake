4171401c0fd479ffbd55ee8d7d84e495304200ff
richardicooper <richardiancooper@gmail.com>
2016-09-06 16:37:22 +0000

    Changes to allow Mac build. MinGW compatibility to be tested asap. Crystals intermediate build folder renamed to crys_fortran to avoid clash with the executable file name 'crystals' on unix platforms. Use find_package(BLAS) to find our libraries (to be tested on MinGW asap).

--- datafiles/dscbuild.cmake
+++ datafiles/dscbuild.cmake
@@ -1,7 +1,8 @@
 
 if(APPLE)
-SET (ENV{CRYSDIR} ./)
-EXECUTE_PROCESS (COMMAND ../crystals.app/Contents/MacOS/crystals ./ )
+SET (ENV{CRYSDIR} "./")
+EXECUTE_PROCESS (COMMAND ../crystals )
+# EXECUTE_PROCESS (COMMAND ../crystals.app/Contents/MacOS/crystals ./ )
 else()
 SET (ENV{CRYSDIR} .\\ )
 EXECUTE_PROCESS (COMMAND ../crystals.exe .\\ )
