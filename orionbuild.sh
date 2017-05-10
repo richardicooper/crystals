169cee7e609b342712679c4adf6cc80c17d7a7b3
pascal22p <pascal22p@parois.net>
2017-05-10 14:53:48 +0000

    Fixed cmake externalProject, everything has to be passed as arguments

--- orionbuild.sh
+++ orionbuild.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cmake ../ -DCMAKE_LIBRARY_PATH=/files/pparois/root/lib/ -DCMAKE_Fortran_COMPILER=gfortran71 -DCMAKE_C_COMPILER=gcc71 -DCMAKE_CXX_COMPILER=g++71 -DCMAKE_BUILD_TYPE=debug
+cmake ../ -DwithSUPERFLIP=YES -DwithEDMA=YES -DCMAKE_LIBRARY_PATH=/files/pparois/root/lib/ -DCMAKE_Fortran_COMPILER=gfortran71 -DCMAKE_C_COMPILER=gcc71 -DCMAKE_CXX_COMPILER=g++71 -DCMAKE_BUILD_TYPE=debug
+
