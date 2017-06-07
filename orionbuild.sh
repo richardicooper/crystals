099c9f87227dfdbf4932b410b115147ff59c73b0
pascal22p <pascal22p@parois.net>
2017-06-07 14:26:10 +0000

    filters folders for documentation, new flag in cmake shortcut for orion

--- orionbuild.sh
+++ orionbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cmake ../ -DwithSUPERFLIP=YES -DwithEDMA=YES -DCMAKE_LIBRARY_PATH=/files/pparois/root/lib/ -DCMAKE_Fortran_COMPILER=gfortran71 -DCMAKE_C_COMPILER=gcc71 -DCMAKE_CXX_COMPILER=g++71 -DCMAKE_BUILD_TYPE=debug
+cmake ../ -Dverbose=YES -DwithSUPERFLIP=YES -DwithEDMA=YES -DCMAKE_LIBRARY_PATH=/files/pparois/root/lib/ -DCMAKE_Fortran_COMPILER=gfortran71 -DCMAKE_C_COMPILER=gcc71 -DCMAKE_CXX_COMPILER=g++71 -DCMAKE_BUILD_TYPE=debug
 
