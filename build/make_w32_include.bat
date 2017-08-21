66ce99d0227215e8fe76882ec8767134d285fd03
richardicooper <richardiancooper@gmail.com>
2017-08-21 06:35:24 +0000

    Oops. I meant /Od, not /O0. Latter option is only available with more recent compiler versions.

--- build/make_w32_include.bat
+++ build/make_w32_include.bat
@@ -78,7 +78,7 @@ goto ALLDVF
 @set F77=ifort
 @set FDEF=%FDEF%
 @set FOPTS=/fpp /I..\crystals /MD /O2 /QaxSSE2 /fp:source /Zi /Oy- /Qdiag-disable:8290 /Qdiag-disable:8291 /nolink %COPENMP%
-@set FNOOPT=/fpp /I..\crystals /MD -O0 /fp:source /Qdiag-disable:8290 /Qdiag-disable:8291 /nolink
+@set FNOOPT=/fpp /I..\crystals /MD -Od /fp:source /Qdiag-disable:8290 /Qdiag-disable:8291 /nolink
 @set FWIN=/winapp
 @set FOUT=/object:obj\
 @set FDEBUG=/fpp /I..\crystals /MDd /debug /fp:source /check:bounds /check:format /check:overflow /check:pointers /check:uninit  /warn:argument_checking /warn:nofileopt /Qdiag-disable:8290 /Qdiag-disable:8291 /nolink /traceback /Qtrapuv
