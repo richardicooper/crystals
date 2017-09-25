5dff119ad168e2c3b108d62533c77ee45a6d4a3f
pascal22p <pascal22p@parois.net>
2017-09-25 10:41:05 +0000

    improved build command on orion (pick the right openblas)

--- orionbuild.sh
+++ orionbuild.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cmake ../ -Dverbose=YES -DwithSUPERFLIP=YES -DwithEDMA=YES -DCMAKE_LIBRARY_PATH=/files/pparois/root/lib/ -DCMAKE_Fortran_COMPILER=gfortran71 -DCMAKE_C_COMPILER=gcc71 -DCMAKE_CXX_COMPILER=g++71 -DCMAKE_BUILD_TYPE=debug
+cmake3 ../ -Dverbose=YES -DwithSUPERFLIP=YES -DwithEDMA=YES -DCMAKE_LIBRARY_PATH=/files/pparois/root/lib/ -DCMAKE_Fortran_COMPILER=gfortran71 -DCMAKE_C_COMPILER=gcc71 -DCMAKE_CXX_COMPILER=g++71 -DBLA_VENDOR=OpenBLAS -DBLAS_LIBRARIES=/files/pparois/root/lib/libopenblas.so -DLAPACK_LIBRARIES=/files/pparois/root/lib/libopenblas.so -DCMAKE_BUILD_TYPE=debug
 
