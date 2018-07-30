9ae8dcf84be0deb864f0a31aa0f00f04a65c157c
pascal22p <pascal22p@parois.net>
2018-07-30 15:36:47 +0000

    fixed end of record bug

--- crystals/list12_helper.F90
+++ crystals/list12_helper.F90
@@ -373,8 +373,12 @@ function printparam(self)
 implicit none
 class(param_t), intent(in) :: self
 character(len=:), allocatable :: printparam
+character(len=512) :: tempa
+character(len=1024) :: tempb
 
-write(printparam, *) '[', self%indices, '] ', trim(self%label), ' ', self%serial, ' ', trim(self%name)
+write(tempa, '(I0,:,",")') self%indices
+write(tempb,'(A,A,A,A,1X,I4,1X,A)') '[', trim(tempa), '] ', trim(self%label), self%serial, trim(self%name)
+printparam=trim(tempb)
 
 end function
 
