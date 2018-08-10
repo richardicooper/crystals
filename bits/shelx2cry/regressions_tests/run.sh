61d3f2eec4ac8eb8e0ba4540238501d0d1ac1890
pascal22p <pascal22p@parois.net>
2018-08-10 09:20:31 +0000

    updated regression tests

--- bits/shelx2cry/regressions_tests/run.sh
+++ bits/shelx2cry/regressions_tests/run.sh
@@ -8,8 +8,8 @@ for path in insfiles/*.ins; do
     echo $file
     ../shelx2cry -o "${file}.out" -l "${file}.log" $path > /dev/null
 
-    diff -q "${file}.out" "references/${file}.out"
-    diff -q "${file}.log" "references/${file}.log"
+    diff  --strip-trailing-cr -q "${file}.out" "references/${file}.out"
+    diff  --strip-trailing-cr -q "${file}.log" "references/${file}.log"
 
 done
 
