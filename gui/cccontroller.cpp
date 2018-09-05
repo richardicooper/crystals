1ff7bec99d1bd9c721e113cdbaa6ddf1419524d2
pascal22p <pascal22p@parois.net>
2018-09-05 09:16:34 +0000

    fixed syntax error. trying different quotes.

--- gui/cccontroller.cpp
+++ gui/cccontroller.cpp
@@ -4558,7 +4558,7 @@ std::string GetExePath() {
   std::string crysdir = path + string("/");
   return crysdir;
 #else
-  std::string crysdir = '';
+  std::string crysdir = "";
   return crysdir;
 #endif     
 }
